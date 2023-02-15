8ed9b416a015e89b8703f855e9b4823fe32cd901
ahn0min <ahn0min95@gmail.com>
2023-02-15 18:26:28 +0900

    #3 [ADD] Recoil 설치 & 세팅 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import {RecoilRoot} from "recoil";
+import type {AppProps} from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "@/styles/globals.css";
+
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
