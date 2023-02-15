282acf322f587b0837b52c0a79452b4f931fd04d
ahn0min <ahn0min95@gmail.com>
2023-02-15 19:06:17 +0900

    #1 [ADD] ESLint & Prettier 설정 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import {RecoilRoot} from "recoil";
-import type {AppProps} from "next/app";
+import { RecoilRoot } from 'recoil';
+import type { AppProps } from 'next/app';
 
-import "@/styles/globals.css";
+import '@/styles/globals.css';
 
-export default function App({Component, pageProps}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Component {...pageProps} />
