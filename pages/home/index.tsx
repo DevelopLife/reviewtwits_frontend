b03168224c339b48668321729ee9c35dbfd10541
ahn0min <ahn0min95@gmail.com>
2023-02-15 22:14:59 +0900

    #3 [ADD] Emotion 설정 완료

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,24 +1,36 @@
+import styled from '@emotion/styled';
 import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
 import { GetServerSideProps } from 'next';
-
-const QUERY_KEY = ['mock'];
-
-const promiseFunction = new Promise((resolve) =>
-  setTimeout(() => resolve({ content: 'success' }), 2000)
-);
-
-const apiFunction = async () => {
-  return (await promiseFunction) as { content: string };
-};
 
 const HomePage = () => {
   const mockQuery = useQuery(QUERY_KEY, apiFunction);
 
-  return <div>{mockQuery.data?.content}</div>;
+  return (
+    <section>
+      <S.Box isLoading>{mockQuery.data?.content}</S.Box>
+    </section>
+  );
 };
 
 export default HomePage;
 
+const S = {
+  Box: styled.div<{ isLoading: boolean }>`
+    background-color: red;
+  `,
+};
+
+// mock fetch function
+const promiseFunction = new Promise((resolve) =>
+  setTimeout(() => resolve({ content: 'success' }), 2000)
+);
+const apiFunction = async () => {
+  return (await promiseFunction) as { content: string };
+};
+
+const QUERY_KEY = ['mock'];
+
+// SSR
 export const getServerSideProps: GetServerSideProps = async () => {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery(QUERY_KEY, apiFunction);
