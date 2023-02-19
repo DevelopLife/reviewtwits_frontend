22c8f190fbf6c099bec6de4971183ea07e6e544c
Jisu00 <jisudev00@gmail.com>
2023-02-19 21:01:13 +0900

    #11 [ADD] reset css 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import {
   QueryClient,
   Hydrate,
 } from '@tanstack/react-query';
+import { Global } from '@emotion/react';
 // only development
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
 
 import '@/styles/globals.css';
 import { useState } from 'react';
+import reset from '@/styles/reset';
 
 export default function App({ Component, pageProps }: AppProps) {
   // TODO: consider setting defaultOptions
@@ -17,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Global styles={reset} />
       <ReactQueryDevtools initialIsOpen={false} />
       <Hydrate state={pageProps.dehydratedState}>
         <RecoilRoot>
