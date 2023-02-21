7d76ad3a384028b5ae9ba281c11c05b54927951d
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-21 19:17:36 +0900

    [ADD] storybook stories 경로 추가
    components 내부 폴더 경로 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '../stories/*.stories.@(js|jsx|ts|tsx)',
     '../components/*.mdx',
     '../components/*.stories.@(js|jsx|ts|tsx)',
+    '../components/*/*.stories.@(js|jsx|ts|tsx)',
   ],
 
   addons: [
