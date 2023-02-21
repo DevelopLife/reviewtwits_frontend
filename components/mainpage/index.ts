0421dbc31c4b892f590076c773372e45bab6c4f6
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-21 20:59:14 +0900

    [FEAT] Mainpage export용 파일 추가

---
+++ components/mainpage/index.ts
@@ -0,0 +1,6 @@
+import EffectIntroduce from './EffectIntroduce/EffectIntroduce';
+import FunctionIntroduce from './FunctionIntroduce/FunctionIntroduce';
+import MainHome from './MainHome/MainHome';
+import ServiceIntroduce from './ServiceIntroduce/ServiceIntroduce';
+
+export { EffectIntroduce, FunctionIntroduce, MainHome, ServiceIntroduce };
