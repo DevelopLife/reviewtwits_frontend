d35f58b51da66a0962a979520a315529452b86d1
Jisu <62230430+Jisu00@users.noreply.github.com>
2023-02-21 17:04:52 +0900

    [SETTING] Global Style 세팅 (#12)
    * [FIX] ESLint + prettier EOL 관련 에러 해결
    
    * [ADD] Pretendard 웹폰트 link 추가
    
    * #11 [ADD] reset css 설정
    
    * #11 [ADD] theme 추가
    
    * #11 [ADD] Title, Body, Line color 지정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
