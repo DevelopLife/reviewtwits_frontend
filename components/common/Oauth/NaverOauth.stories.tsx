cfa9a5bb35620e4f45a822f29084a92ba6b09450
YeongMin Ahn <ahn0min95@gmail.com>
2023-02-25 01:34:50 +0900

    #8 [FEAT] 네이버 로그인 및 회원 정보 받아오기

---
+++ components/common/Oauth/NaverOauth.stories.tsx
@@ -0,0 +1,10 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { NaverOauth } from '@/components/common/Oauth/NaverOauth';
+
+export default {
+  title: 'Components/Oauth/NaverOauth',
+  component: NaverOauth,
+} as ComponentMeta<typeof NaverOauth>;
+
+export const Primary: ComponentStory<typeof NaverOauth> = () => <NaverOauth />;
