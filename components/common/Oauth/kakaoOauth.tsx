aa16334968a241edb65d23c8b51d5d244bde3756
Jisu00 <jisudev00@gmail.com>
2023-02-24 11:30:43 +0900

    #7 [FEAT] 카카오 회원 정보 받아오기

---
+++ components/common/Oauth/kakaoOauth.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+import styled from '@emotion/styled';
+
+import KakaoIcon from '@public/images/kakao_icon.svg';
+
+export const KakaoOauth = () => {
+  return <KakaoLoginButton />;
+};
+
+export const KakaoLoginButton = () => {
+  function loginWithKakao() {
+    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID);
+    window.Kakao.Auth.authorize({
+      redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+    });
+  }
+
+  return <KakaoLoginButtonView handleClick={loginWithKakao} />;
+};
+
+interface KakaoLoginButtonViewProps {
+  handleClick: () => void;
+}
+
+const KakaoLoginButtonView = ({ handleClick }: KakaoLoginButtonViewProps) => {
+  return (
+    <S.Button id="kakao-login-btn" onClick={handleClick}>
+      <Image src={KakaoIcon} width={60} height={60} alt="kakao_icon" />
+    </S.Button>
+  );
+};
+
+const S = {
+  Button: styled.div``,
+};
