d84e069cf0923e621a1d4dc201b8e39541b9da16
YeongMin Ahn <ahn0min95@gmail.com>
2023-02-25 21:48:10 +0900

    [FIX] 대소문자로 인한 깃 에러 해결

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   // stories 파일을 포함하고 싶은 폴더가 존재하면 해당 부분을 수정해주세요.
   stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
-
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
