448b916e1c138d488d5d1afbc157503f933920eb
Juhyun <boochickenood@gmail.com>
2023-02-26 23:36:39 +0900

    [CHORE] 중첩된 폴더 구조 제거
    사용하지 않는 파일 제거

--- components/mainpage/index.ts
+++
@@ -1,6 +0,0 @@
-import EffectIntroduce from './EffectIntroduce/EffectIntroduce';
-import FunctionIntroduce from './FunctionIntroduce/FunctionIntroduce';
-import MainHome from './MainHome/MainHome';
-import ServiceIntroduce from './ServiceIntroduce/ServiceIntroduce';
-
-export { EffectIntroduce, FunctionIntroduce, MainHome, ServiceIntroduce };
