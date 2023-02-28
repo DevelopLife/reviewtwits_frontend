77629de00915e1827fd838ec1757762485e128bd
Juhyun <boochickenood@gmail.com>
2023-02-28 23:21:39 +0900

    [ADD] mainpage slider 구현

--- components/mainpage/MainHome.styles.ts
+++ components/mainpage/MainHome.styles.ts
@@ -1,9 +1,30 @@
 import styled from '@emotion/styled';
 
-const Container = styled.div`
-  width: ${({ theme }) => theme.devices.desktop};
+const SliderView = styled.div`
+  width: ${({ theme }) => theme.devices.desktop}px;
   height: ${1011 + 69}px;
-  background-color: ${({ theme }) => theme.colors.red_0};
+  overflow: hidden;
+`;
+
+const SliderContainer = styled.div<{
+  sliderCount: number;
+  currentSlideIndex: number;
+}>`
+  display: flex;
+  width: ${(props) => props.theme.devices.desktop * props.sliderCount}px;
+  transform: translateX(
+    ${(props) => -1 * props.theme.devices.desktop * props.currentSlideIndex}px
+  );
+  transition: ${(props) =>
+    props.currentSlideIndex !== 0
+      ? 'all 500ms ease-in-out'
+      : 'all 0ms ease-in-out'};
+`;
+
+const Container = styled.div<{ backgroundColor: string }>`
+  width: ${({ theme }) => theme.devices.desktop}px;
+  height: ${1011 + 69}px;
+  background-color: ${(props) => props.backgroundColor};
 `;
 
 const MainInfoArea = styled.div`
@@ -14,26 +35,29 @@ const LeftArea = styled.div`
   margin-left: 240px;
 `;
 
-const AppInfo = styled.h1`
+const AppInfo = styled.h1<{ infoColor: string }>`
   margin-top: ${69 + 389}px;
-
-  font-family: 'Pretendard';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 66px;
-  line-height: 80px;
-  color: #ffffff;
-
   margin-bottom: 47px;
+
+  div {
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 66px;
+    line-height: 80px;
+    /* color: #ffffff; */
+    color: ${(props) => props.infoColor};
+  }
 `;
-const AppDesc = styled.p`
+const AppDesc = styled.p<{ descColor: string }>`
   font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
   font-size: 22px;
   line-height: 36px;
 
-  color: #ededed;
+  /* color: #ededed; */
+  color: ${(props) => props.descColor};
 `;
 
 const RightArea = styled.div`
@@ -68,6 +92,8 @@ const Button = styled.div`
 `;
 
 export {
+  SliderContainer,
+  SliderView,
   Container,
   MainInfoArea,
   LeftArea,
