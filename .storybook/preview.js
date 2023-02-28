9e0553e8f4c108c0715c91b9300cf8dd827916db
JuhyunLim <boochickenood@gmail.com>
2023-02-28 22:59:47 +0900

    [SETTING] storybook에 Global style과 theme 세팅 (#31)
    * [SETTING] storybook에 Global style과 theme 세팅
    
    * [FIX] preview.js에서 import @ 삭제
    
    cromatic conflict 해결

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,18 @@
+import { Global, ThemeProvider } from '@emotion/react';
+import reset from '/styles/reset';
+import theme from '/styles/theme';
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Global styles={reset} />
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    </>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
