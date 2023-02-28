02a1ddd11066a1f318c4a827b6a7170a376a70ec
Juhyun <boochickenood@gmail.com>
2023-02-28 23:08:20 +0900

    [FIX] preview.js import 절대경로 @ 제거

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import { Global, ThemeProvider } from '@emotion/react';
-import reset from '@/styles/reset';
-import theme from '@/styles/theme';
+import reset from '/styles/reset';
+import theme from '/styles/theme';
 import * as NextImage from 'next/image';
 
 const OriginalNextImage = NextImage.default;
