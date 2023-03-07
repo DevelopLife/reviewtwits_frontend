f26676424ead2f51bac8579613f36bad040a8d4b
YeongMin Ahn <ahn0min95@gmail.com>
2023-03-08 02:43:05 +0900

    #36 [STYLE] 프로젝트 플랜 카드 스타일 작업

--- components/common/PlanCard/PlanCard.tsx
+++ components/common/PlanCard/PlanCard.tsx
@@ -1,13 +1,37 @@
 import styled from '@emotion/styled';
+import Image from 'next/image';
 
-export const PlanCard = () => {
-  return <PlanCardView />;
+import CheckedIconSVG from 'public/images/checked_icon.svg';
+
+interface PlanCardProps {
+  title: string;
+  price: number;
+  options: string[];
+}
+
+export const PlanCard = ({ title, price, options }: PlanCardProps) => {
+  return <PlanCardView title={title} price={price} options={options} />;
 };
-export const PlanCardView = () => {
+
+export const PlanCardView = ({ title, price, options }: PlanCardProps) => {
+  const planPrice = price === -1 ? 'custom pricing' : `$${price} per month`;
   return (
     <S.PlanCardContainer>
-      <S.PlanTitle>Plan</S.PlanTitle>
-      <S.Plans></S.Plans>
+      <S.Title>{title}</S.Title>
+      <S.Price>{planPrice}</S.Price>
+      <S.Plans>
+        {options.map((option) => (
+          <S.PlanWrap key={option}>
+            <Image
+              src={CheckedIconSVG}
+              alt={'checkedIcon'}
+              width={25}
+              height={25}
+            />
+            <S.Plan>{option}</S.Plan>
+          </S.PlanWrap>
+        ))}
+      </S.Plans>
     </S.PlanCardContainer>
   );
 };
@@ -17,7 +41,7 @@ const S = {
     width: 273px;
     height: 369px;
     border-radius: 15px;
-    padding: 56px 33px;
+    padding: 56px 25px 56px 29px;
 
     :hover {
       cursor: pointer;
@@ -27,15 +51,37 @@ const S = {
     border: ${() => `1px solid ${Colors.border}`};
     background-color: ${() => Colors.backgroundColor.selected};
   `,
-  PlanTitle: styled.h2`
+  Title: styled.h2`
     font-weight: 700;
     font-size: 28px;
     line-height: 34px;
 
     color: ${() => Colors.color.selected};
   `,
-  Plans: styled.ul``,
-  Plan: styled.li``,
+  Price: styled.p`
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22px;
+    color: ${() => Colors.priceColor.primary};
+  `,
+  Plans: styled.ul`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10px;
+    margin-top: 35px;
+  `,
+  PlanWrap: styled.li`
+    display: flex;
+    align-items: flex-end;
+    gap: 2px;
+  `,
+  Plan: styled.span`
+    font-weight: 500;
+    font-size: 16px;
+    vertical-align: bottom;
+    color: ${() => Colors.optionsColor.selected};
+  `,
 };
 
 const Colors = {
@@ -44,8 +90,15 @@ const Colors = {
     primary: '#181818',
     selected: '#FFFFFF',
   },
+  priceColor: {
+    primary: '#878D91',
+  },
+  optionsColor: {
+    primary: '#181818',
+    selected: '#FFFFFF',
+  },
   backgroundColor: {
     primary: '#FFFFFF',
-    selected: '#181818',
+    selected: '#393939',
   },
 };
