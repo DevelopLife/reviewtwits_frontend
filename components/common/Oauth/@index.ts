30f28a095890b68d317a30229af1d0dc02f8e6f2
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-03-10 23:09:33 +0900

    [ADD] CI/CD test3

--- components/common/Oauth/@index.ts
+++ components/common/Oauth/@index.ts
@@ -1,5 +1,4 @@
 import { GoogleOauth } from 'components/common/Oauth/GoogleOauth';
-import { KakaoOauth } from 'components/common/Oauth/KakaoOauth';
 import { NaverOauth } from 'components/common/Oauth/NaverOauth';
 
-export { GoogleOauth, NaverOauth, KakaoOauth };
+export { GoogleOauth, NaverOauth };
