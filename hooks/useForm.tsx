e1cfb6d2909aec0762dd02bfcce939b475122467
Jisu00 <jisudev00@gmail.com>
2023-03-13 14:04:25 +0900

    #24 [DEL] 불필요한 코드 및 파일 제거

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -6,8 +6,7 @@ import {
   useEffect,
   useState,
 } from 'react';
-import { signAPI } from 'api/sign';
-import type { SignUpParams, UserFormType } from 'typings/account';
+import type { UserFormType } from 'typings/account';
 
 const useForm = (initialValues: UserFormType) => {
   const [values, setValues] = useState<UserFormType>(initialValues);
@@ -43,14 +42,7 @@ const useForm = (initialValues: UserFormType) => {
   }: ChangeEvent<HTMLInputElement> | MouseEvent<HTMLButtonElement>) => {
     const { name, value } = currentTarget;
 
-    if (name === 'birthday') {
-      const birthDate = convertStringToDate(value);
-      const newValues = { ...values, [name]: birthDate };
-      return setValues(newValues);
-    }
-
     const newValues = { ...values, [name]: value };
-
     setValues(newValues);
   };
 
@@ -60,30 +52,7 @@ const useForm = (initialValues: UserFormType) => {
   ) => {
     e.preventDefault();
 
-    // passwordCheck를 제거
-    const signUpParams: SignUpParams = {
-      nickname: '테스트닉네임',
-      accountId: values.accountId,
-      authenticationCode: '성공',
-      accountPw: values.accountPw,
-      phoneNumber: values.phoneNumber,
-      birthday: values.birthday,
-      gender: values.gender,
-    };
-
-    const response = await signAPI.signUp(signUpParams);
     onValid();
-  };
-
-  const convertStringToDate = (dateString: string) => {
-    const dates = dateString
-      .split('-')
-      .map((dateElement) => Number(dateElement));
-
-    const [year, month, date] = dates;
-    const convertedDate = new Date(year, month - 1, date);
-
-    return convertedDate;
   };
 
   useEffect(() => {
