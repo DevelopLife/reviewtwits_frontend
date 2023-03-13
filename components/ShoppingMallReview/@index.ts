e1ebdc6d418745ab784db8ed962a15c989dd88ad
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-03-13 15:08:03 +0900

    [FEAT] ShoppingMallReview 폴더의 컴포넌트 한번에 export

---
+++ components/ShoppingMallReview/@index.ts
@@ -0,0 +1,7 @@
+import ComprehensiveRates from './ComprehensiveRates';
+import KeywordRates from './KeywordRates';
+import ReviewHeader from './ReviewHeader';
+import Reviews from './Reviews';
+import Review from './Review';
+
+export { ComprehensiveRates, KeywordRates, ReviewHeader, Reviews, Review };
