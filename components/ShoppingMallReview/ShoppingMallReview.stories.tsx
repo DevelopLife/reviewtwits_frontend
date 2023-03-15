561e365382f63a5bffd55bd454f7d6cf1ea6fb32
Juhyun <boochickenood@gmail.com>
2023-03-15 23:12:48 +0900

    [ADD] ShoppingMallReview 스토리북 추가

---
+++ components/ShoppingMallReview/ShoppingMallReview.stories.tsx
@@ -0,0 +1,8 @@
+import ShoppingMallReview from './ShoppingMallReview';
+
+export default {
+  title: 'ShoppingMallReview',
+  component: ShoppingMallReview,
+};
+
+export const Basic = () => <ShoppingMallReview />;
