cad0adc0e1698c658a76fcde22d9068ade07838c
Juhyun <boochickenood@gmail.com>
2023-03-15 10:07:17 +0900

    [ADD] ShoppingMallReview.tsx 전체 레이아웃 스타일 작업

--- components/ShoppingMallReview/ShoppingMallReview.styles.ts
+++ components/ShoppingMallReview/ShoppingMallReview.styles.ts
@@ -5,4 +5,30 @@ const Layout = styled.div`
   border: 1px solid black;
 `;
 
-export { Layout };
+const ComprehensiveRatesLayout = styled.div`
+  margin-top: 40px;
+  margin-left: 127px;
+`;
+const KeywordRatesLayout = styled.div`
+  margin-top: 40px;
+  margin-left: 127px;
+`;
+
+const ReviewHeaderLayout = styled.div`
+  margin-top: 67px;
+  margin-left: 50px;
+`;
+const ReviewsLayout = styled.div`
+  margin-top: 32px;
+  margin-bottom: 69px;
+
+  margin-left: 74px;
+`;
+
+export {
+  Layout,
+  ComprehensiveRatesLayout,
+  KeywordRatesLayout,
+  ReviewHeaderLayout,
+  ReviewsLayout,
+};
