e30459815ea9eea4097e22d12e734105a27a7919
Juhyun <boochickenood@gmail.com>
2023-03-15 10:09:01 +0900

    [FIX] shopping-mall-review와 merge 된 부분 제거

--- components/ShoppingMallReview/@index.ts
+++
@@ -1,7 +0,0 @@
-import ComprehensiveRates from './ComprehensiveRates';
-import KeywordRates from './KeywordRates';
-import ReviewHeader from './ReviewHeader';
-import Reviews from './Reviews';
-import Review from './Review';
-
-export { ComprehensiveRates, KeywordRates, ReviewHeader, Reviews, Review };
--- components/ShoppingMallReview/ShoppingMallReview.styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-const Layout = styled.div`
-  width: 1200px;
-  border: 1px solid black;
-`;
-
-export { Layout };
