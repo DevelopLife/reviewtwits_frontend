eab3bc45716bcf430df52d17cb60247966aed266
Jisu <62230430+Jisu00@users.noreply.github.com>
2023-03-16 15:39:52 +0900

    [SETTING] emotion Theme type 세팅 (#29)
    * [FIX] ESLint + prettier EOL 관련 에러 해결
    
    * [ADD] Pretendard 웹폰트 link 추가
    
    * #11 [ADD] reset css 설정
    
    * #11 [ADD] theme 추가
    
    * #11 [ADD] Title, Body, Line color 지정
    
    * #7 [ADD] axios instance 생성
    
    * [ADD] api 폴더 paths 추가
    
    * #22 [FEAT] 구글 로그인 및 프로필 정보 받아오기
    
    * #22 [ADD] GooglaOauth Stories
    
    * #7 [FEAT] 카카오 회원 정보 받아오기
    
    * #7 [ADD] KakaoOauth Story 추가
    
    * [DEL] reset css에 font inherit 속성 제거
    
    * #28 [ADD] Colors key type 추가
    
    ---------
    
    Co-authored-by: YeongMin Ahn <ahn0min95@gmail.com>

--- styles/reset.ts
+++ styles/reset.ts
@@ -88,7 +88,6 @@ const reset = css`
     padding: 0;
     border: 0;
     font-size: 100%;
-    font: inherit;
     vertical-align: baseline;
   }
   /* HTML5 display-role reset for older browsers */
