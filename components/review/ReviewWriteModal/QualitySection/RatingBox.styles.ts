befe73ca786e5713a36a999539dce7db626dab20
Jisu00 <jisudev00@gmail.com>
2023-03-17 20:42:43 +0900

    #40 [FEAT] 별점 기능 구현 및 애니메이션 추가

--- components/review/ReviewWriteModal/QualitySection/RatingBox.styles.ts
+++ components/review/ReviewWriteModal/QualitySection/RatingBox.styles.ts
@@ -1,4 +1,9 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+interface StarStyleProps {
+  isActive: boolean;
+}
 
 const Box = styled.div`
   display: flex;
@@ -25,13 +30,28 @@ const StarRating = styled.div`
   gap: 5px;
 `;
 
-const Star = styled.button`
+const Star = styled.button<StarStyleProps>`
   outline: none;
   background: none;
   border: none;
   padding: 0;
 
   cursor: pointer;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      @keyframes expand {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(1.2);
+        }
+      }
+
+      animation: expand 0.3s reverse;
+    `}
 `;
 
 export { Box, ProductName, StarRating, Star };
--- components/review/ReviewWriteModal/QualitySection/RatingBox.tsx
+++ components/review/ReviewWriteModal/QualitySection/RatingBox.tsx
@@ -1,3 +1,4 @@
+import { useState, MouseEvent } from 'react';
 import Image from 'next/image';
 
 import * as S from './RatingBox.styles';
@@ -5,6 +6,23 @@ import FullStarImg from 'public/images/full_star_img.png';
 import EmptyStarImg from 'public/images/empty_star_img.png';
 
 const RatingBox = () => {
+  const [score, setScore] = useState(0);
+  const [hoverScore, setHoverScore] = useState(0);
+
+  const changeScore = (e: MouseEvent<HTMLButtonElement>) => {
+    const selectedStarNumber = Number(e.currentTarget.id);
+
+    setScore(selectedStarNumber);
+  };
+
+  const handleHoverStar = (e: MouseEvent<HTMLButtonElement>) => {
+    const selectedStarNumber = Number(e.currentTarget.id);
+
+    setHoverScore(selectedStarNumber);
+  };
+
+  const handleLeaveStar = () => setHoverScore(score);
+
   return (
     <S.Box>
       <Image
@@ -16,15 +34,21 @@ const RatingBox = () => {
       />
       <div>
         <S.ProductName>오뚜기 콤비네이션 피자 415g 오뚜기, 2개</S.ProductName>
-        <S.StarRating>
-          {[1, 2, 3].map((_, i) => (
-            <S.Star key={i}>
-              <Image width={30} height={30} src={FullStarImg} alt="" />
-            </S.Star>
-          ))}
-          {[1, 2].map((_, i) => (
-            <S.Star key={i}>
-              <Image width={30} height={30} src={EmptyStarImg} alt="" />
+        <S.StarRating onMouseLeave={handleLeaveStar}>
+          {Array.from({ length: 5 }).map((_, i) => (
+            <S.Star
+              id={(i + 1).toString()}
+              key={i}
+              isActive={score > i}
+              onClick={changeScore}
+              onMouseEnter={handleHoverStar}
+            >
+              <Image
+                width={30}
+                height={30}
+                src={hoverScore <= i ? EmptyStarImg : FullStarImg}
+                alt=""
+              />
             </S.Star>
           ))}
         </S.StarRating>
