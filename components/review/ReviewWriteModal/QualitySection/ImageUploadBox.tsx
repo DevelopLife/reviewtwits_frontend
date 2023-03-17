da71983b14d72a3bd4b0a6d971c48b046c0fb23e
Jisu00 <jisudev00@gmail.com>
2023-03-17 21:24:06 +0900

    #40 [FEAT] '사진 올리기' 버튼 클릭 시 파일 열기 모달 띄우도록 구현

--- components/review/ReviewWriteModal/QualitySection/ImageUploadBox.tsx
+++ components/review/ReviewWriteModal/QualitySection/ImageUploadBox.tsx
@@ -1,16 +1,22 @@
+import { useRef } from 'react';
 import Image from 'next/image';
 
 import * as S from './ImageUploadBox.styles';
 import CameraIcon from 'public/images/camera_icon.svg';
 
 const ImageUploadBox = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClickImageUpload = () =>
+    inputRef.current && inputRef.current.click();
+
   return (
     <S.ImageUploadBox>
-      <S.ImageUploadButton>
+      <S.ImageUploadButton onClick={handleClickImageUpload}>
         <Image width={24} height={24} src={CameraIcon} alt="" />
         사진 올리기
       </S.ImageUploadButton>
-      <input type="file" accept="image/*" />
+      <input ref={inputRef} type="file" accept="image/*" />
       <S.ImageList>
         {[1, 2, 3].map((_, i) => (
           <Image
