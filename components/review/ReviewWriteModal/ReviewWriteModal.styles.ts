0fdb4ed39051ad37af2c57816d2cc2789a62627d
Jisu00 <jisudev00@gmail.com>
2023-03-17 18:12:50 +0900

    #40 [ADD] 리뷰 작성 모달 layout 구현

--- components/review/ReviewWriteModal/ReviewWriteModal.styles.ts
+++ components/review/ReviewWriteModal/ReviewWriteModal.styles.ts
@@ -1,3 +1,26 @@
 import styled from '@emotion/styled';
+import theme from 'styles/theme';
 
-export {};
+const Container = styled.div`
+  width: 1000px;
+  padding: 60px;
+`;
+
+const Title = styled.h1`
+  font-weight: 500;
+  font-size: 36px;
+  margin: 13px 24px;
+`;
+
+const TitleLine = styled.hr`
+  height: 2px;
+  background: ${theme.colors.gray_7};
+  border: none;
+  margin: 0;
+`;
+
+const ReviewContent = styled.div`
+  padding: 0 24px;
+`;
+
+export { Container, Title, TitleLine, ReviewContent };
