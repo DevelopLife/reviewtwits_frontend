bc9ba56235ecf61c02f5a1ddfb90bf3b072023be
Jisu00 <jisudev00@gmail.com>
2023-03-17 19:20:47 +0900

    #40 [ADD] SatisfactionBox 추가

--- components/review/ReviewWriteModal/ServiceSection/@index.tsx
+++ components/review/ReviewWriteModal/ServiceSection/@index.tsx
@@ -1,24 +1,13 @@
-import Image from 'next/image';
+import * as S from '../ReviewWriteModal.styles';
 
-import * as S from '../ServiceSection.styles';
-import ThumbIcon from 'public/images/thumb_icon.svg';
 import ServiceQuestionBox from './ServiceQuestionBox';
+import { SatisfactionBox } from './SatisfactionBox.styles';
 
 const ServiceSection = () => {
   return (
     <S.Section>
       <ServiceQuestionBox />
-      <S.SatisfactionBox>
-        <S.ReviewFor>만족도</S.ReviewFor>
-        <S.ThumbButtonBox>
-          <S.ThumbsUpButton>
-            <Image width={30} height={30} src={ThumbIcon} alt="" />
-          </S.ThumbsUpButton>
-          <S.ThumbsDownButton>
-            <Image width={30} height={30} src={ThumbIcon} alt="" />
-          </S.ThumbsDownButton>
-        </S.ThumbButtonBox>
-      </S.SatisfactionBox>
+      <SatisfactionBox />
     </S.Section>
   );
 };
