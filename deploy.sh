892c1015329c9042fa032aa344cf49ba7764eead
Juhyun <boochickenood@gmail.com>
2023-03-17 17:51:07 +0900

    [ADD] CI/CD test 28

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
+sudo apt install yarn -y
+
 yarn install
 
 nohup yarn start >> $REPOSITORY/nohup.out 2>&1 &
