215ad7b442cba095106d4ef37b4675493434cbf8
Jisu00 <jisudev00@gmail.com>
2023-03-19 20:23:52 +0900

    [REFACTOR] validate util에서 regExp 분리

--- utils/validate.ts
+++ utils/validate.ts
@@ -1,8 +1,4 @@
-const regExp = {
-  email:
-    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
-  password: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@!%*#?&])[A-Za-z\d@!%*#?&]{6,}$/,
-};
+import { regExp } from './regExp';
 
 export const validateEmail = (email: string) => regExp.email.test(email);
 export const validatePhoneNumber = (tel: string) => !isNaN(Number(tel));
