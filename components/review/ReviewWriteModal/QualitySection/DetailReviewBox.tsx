71d30c7278602ecf526f8b75a97d5d00029a845b
Jisu00 <jisudev00@gmail.com>
2023-03-20 14:02:59 +0900

    [ADD] 상세 리뷰 placeholder 수정

--- components/review/ReviewWriteModal/QualitySection/DetailReviewBox.tsx
+++ components/review/ReviewWriteModal/QualitySection/DetailReviewBox.tsx
@@ -5,7 +5,7 @@ const DetailReviewBox = () => {
     <S.Box>
       <S.ReviewFor>상세리뷰</S.ReviewFor>
       <S.TextArea
-        placeholder={`다른 고객님에게 도움이 될 수 있도록 솔직한 평가를 남겨주세요.\n고객님의 피드백이 더 나은 제품 제공되는데 도움이 됩니다\n\n\n\n\n\n\n\n\n\n\n\n상품과 관계 없거나 악의적 내용은 비공개 처리될 수 있습니다.`}
+        placeholder={`다른 고객님에게 도움이 될 수 있도록 솔직한 평가를 남겨주세요.\n고객님의 피드백이 더 나은 제품 제공되는데 도움이 됩니다.\n\n\n\n\n\n\n\n\n\n\n\n상품과 관계 없거나 악의적 내용은 비공개 처리될 수 있습니다.`}
       />
     </S.Box>
   );
