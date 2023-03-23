10dae77d08b890881f6388c07633b788806ea151
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-03-23 13:39:11 +0900

    [DEL] Oauth 관련 storybook 제거

--- components/common/Oauth/NaverOauth.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { NaverOauth } from 'components/common/Oauth/@index';
-
-export default {
-  title: 'Components/Oauth/NaverOauth',
-  component: NaverOauth,
-} as ComponentMeta<typeof NaverOauth>;
-
-export const Primary: ComponentStory<typeof NaverOauth> = () => <NaverOauth />;
