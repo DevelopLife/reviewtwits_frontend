03cc44b3bd1a8681c1ffa3d77b354b9462cc2692
YeongMin Ahn <ahn0min95@gmail.com>
2023-03-27 19:02:19 +0900

    #60 [CHORE] 스토리 파일 타입 수정

--- components/Project/install/CodeParagraph.stories.tsx
+++ components/Project/install/CodeParagraph.stories.tsx
@@ -1,11 +1,11 @@
-import { StoryFn } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/react';
 import CodeParagraph from 'components/Project/install/CodeParagraph';
 
 export default {
   title: 'project/install/CodeParagraph',
   component: CodeParagraph,
-};
+} as Meta<typeof CodeParagraph>;
 
-export const primary: StoryFn = () => (
+export const primary: StoryFn<typeof CodeParagraph> = () => (
   <CodeParagraph text={'1. insert message'} code={'<div>insert code</div>'} />
 );
