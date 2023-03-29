5547ab3922004e12c0d37ace3f6fafee14142b04
YeongMin Ahn <ahn0min95@gmail.com>
2023-03-30 00:19:29 +0900

    [CHORE] code highlight를 위한 코드 변경

--- components/common/HighlightCDN.tsx
+++ components/common/HighlightCDN.tsx
@@ -1,23 +1,24 @@
 import hljs from 'highlight.js';
-import javascript from 'highlight.js/lib/languages/javascript.js';
-hljs.registerLanguage('javascript', javascript);
-
 import Head from 'next/head';
+import Script from 'next/script';
 import { useEffect } from 'react';
+import javascript from 'highlight.js/lib/languages/javascript.js';
+
+hljs.registerLanguage('javascript', javascript);
 
 const HighlightCDN = () => {
   useEffect(() => {
     hljs.initHighlighting();
+    hljs.highlightAll();
   }, []);
 
   return (
     <>
-      <script src="/path/to/highlight.min.js" async></script>
-      <script>hljs.highlightAll();</script>
+      <Script src="/path/to/highlight.min.js"></Script>
       <Head>
         <link
           rel="stylesheet"
-          href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.3.2/build/styles/default.min.css"
+          href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.3.2/build/styles/default.min.css"
         ></link>
       </Head>
     </>
