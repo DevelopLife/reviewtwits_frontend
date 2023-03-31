9651f8ebfef5559516a1ba68d8a2adee812222ac
Jisu <62230430+Jisu00@users.noreply.github.com>
2023-03-31 14:32:46 +0900

    [FEATURE] 4.1 SNS 메인 페이지 구현 (#86)
    * #83 [FEAT] SNS 메인 페이지 구현
    
    * #75 [CHORE] 불필요한 import 제거

---
+++ components/social/main/SocialMainLayout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import styled from '@emotion/styled';
+
+interface SocialMainLayoutProps {
+  children: ReactNode;
+}
+
+const SocialMainLayout = ({ children }: SocialMainLayoutProps) => {
+  return <S.Layout>{children}</S.Layout>;
+};
+
+export default SocialMainLayout;
+
+const S = {
+  Layout: styled.div`
+    display: flex;
+    gap: 40px;
+  `,
+};
