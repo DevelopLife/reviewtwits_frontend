13f9ea53590489acecb7d4a18075e49a69e652f8
Jisu00 <jisudev00@gmail.com>
2023-03-31 18:52:22 +0900

    [ADD] 페이지에 Social Layout 적용

--- pages/social/home/index.tsx
+++ pages/social/home/index.tsx
@@ -1,9 +1,14 @@
 import SocialLayout from 'components/sns/common/SocialLayout';
+import MainContentSection from 'components/social/feed/MainContentSection';
+import UserFilterBar from 'components/social/feed/UserFilterBar';
+import SideBar from 'components/social/feed/SideBar';
 
 const SNSExplorePage = () => {
   return (
     <SocialLayout>
-      <div>home</div>
+      <UserFilterBar />
+      <MainContentSection />
+      <SideBar />
     </SocialLayout>
   );
 };
