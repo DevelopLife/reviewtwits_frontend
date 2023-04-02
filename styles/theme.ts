e12f5a11aa0f8b067973d2ecbce202ebb18f4d3c
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-04-02 21:58:12 +0900

    [ADD] theme.ts colors에 blue_1 추가

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,6 +2,7 @@ const colors = {
   primary: '#FC4A55',
   blue_light: '#87b1f3',
   blue_0: '#4c80f1',
+  blue_1: '#2D87FF',
   blue_dark: '#2c5ae9',
   green_light: '#76e8ad',
   green_0: '#35d48d',
