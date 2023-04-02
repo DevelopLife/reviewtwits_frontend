061131061a5b8fe7126c00752c0e8aeb7555fa61
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-04-03 01:15:32 +0900

    [FEAT] SocialCard를 리스트로 출력하는 SocialList 컴포넌트 제작

---
+++ components/social/Follower/SocialList.tsx
@@ -0,0 +1,12 @@
+import SocialCard from 'components/sns/common/SocialCard';
+import React from 'react';
+
+const SocialList = () => {
+  return (
+    <div>
+      <SocialCard></SocialCard>
+    </div>
+  );
+};
+
+export default SocialList;
