77f8d1a3012a50921ad07d37cfcc54435bd23c0b
Juhyun <boochickenood@gmail.com>
2023-04-03 21:58:10 +0900

    [ADD] ChangeFollowListButton storybook 작성

---
+++ components/social/Follower/ChangeFollowListButton.stories.tsx
@@ -0,0 +1,8 @@
+import ChangeFollowListButton from './ChangeFollowListButton';
+
+export default {
+  title: 'Social/Follower/ChangeFollowListButton',
+  component: ChangeFollowListButton,
+};
+
+export const Basic = () => <ChangeFollowListButton />;
