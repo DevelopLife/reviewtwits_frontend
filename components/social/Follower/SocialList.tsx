15ee4652647c15d40a5f8fd3e293fa75273aacc5
Juhyun <boochickenood@gmail.com>
2023-04-03 21:53:05 +0900

    [FEAT] SocialList 컴포넌트에 임시 사용자 리스트 등록

--- components/social/Follower/SocialList.tsx
+++ components/social/Follower/SocialList.tsx
@@ -1,12 +1,37 @@
+import styled from '@emotion/styled';
 import SocialCard from 'components/sns/common/SocialCard';
 import React from 'react';
 
+const MockDatas = [
+  { name: '1', role: '모델' },
+  { name: '2', role: '모델' },
+  { name: '3', role: '모델' },
+  { name: '4', role: '모델' },
+  { name: '5', role: '모델' },
+];
+
 const SocialList = () => {
   return (
-    <div>
-      <SocialCard></SocialCard>
-    </div>
+    <S.Container>
+      {MockDatas.map(({ name, role }) => (
+        <SocialCard key={name} name={name} role={role} />
+      ))}
+    </S.Container>
   );
+};
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
+    width: 457px;
+    height: auto;
+
+    margin-top: 32px;
+    margin-bottom: 98px;
+  `,
 };
 
 export default SocialList;
