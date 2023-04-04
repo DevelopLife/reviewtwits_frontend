70792fc07a2266f22491dc8aae4fbaabc3c6bd5a
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-04 18:38:28 +0900

    [DEL] mockAPI 제거

--- api/instance.ts
+++ api/instance.ts
@@ -12,13 +12,6 @@ export const authApi = axios.create({
   withCredentials: true,
 });
 
-export const mockApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
-  headers: {
-    'X-AUTH-TOKEN':
-      'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhaG4wbWluOTVAZ21haWwuY29tIiwicm9sZXMiOlsiVVNFUiJdLCJpYXQiOjE2Nzg0NTMwODYsImV4cCI6MTY3ODQ1NjY4Nn0.kxle2cDB6WI9LPhdyluwoLE-v4EVZj1jUdjTbUTC6VI',
-  },
-});
 export const oauthApi = axios.create();
 
 api.interceptors.request.use(verifyToken, verifyTokenErrorHandler);
