21d40d96b757a6139c6121ef0ef8612130c9e09a
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-07 18:17:29 +0900

    [FEATURE] 401 or 403 에러인 경우 로그인 페이지로 일괄 리다이렉트 (#113)
    * #110 [FEAT] 400번대 일괄적인 리다이렉트
    
    * #110 [FEAT] 415 error 핸들러 추가
    
    * #110 [CHORE] alert 문구 변경
    
    * #110 [FIX] api 에러 핸들러 재적용
    
    * #101 [FEAT] 전체경로를 통해 어디서든 동일한 로그인 페이지로 이동하도록 변경
    
    * [DEL] 400번대 에러 일괄처리로 인해 중복되는 에러처리 콜백 제거

---
+++ utils/errorHandler.ts
@@ -0,0 +1,26 @@
+import { AxiosError } from 'axios';
+import { windowNavigate } from 'utils/windowNavigate';
+
+const SIGN_IN = 'sign-in';
+
+export function errorHandler(err: AxiosError<any, any>) {
+  const REDIRECT_URL = `${window?.location.origin}/${SIGN_IN}`;
+  const status = err.response?.status;
+
+  if (!status) {
+    return console.error(status);
+  }
+
+  if (status === 401) {
+    alert('재로그인이 필요해요.');
+    return windowNavigate(REDIRECT_URL);
+  }
+  if (status === 403) {
+    alert('권한이 없어요.');
+    return windowNavigate(REDIRECT_URL);
+  }
+  if (status === 415) {
+    console.warn('header의 content-type을 확인해주세요');
+    return alert('개발자에게 문의해 주세요. reviewTwit@google.com');
+  }
+}
