4e84d37e67a85a9c908e21fa4ea020653495273d
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-09 20:18:04 +0900

    [STYLE] 4줄 이상시 소셜 자기소개 줄임표처리

--- components/social/profile/SocialProfileCard.tsx
+++ components/social/profile/SocialProfileCard.tsx
@@ -7,7 +7,17 @@ export const SocialProfileCard = () => {
   const mockProfileData: SocialProfileCardViewProps = {
     profileImage: '',
     nickName: 'mockNickname',
+    carrer: 'IT 엔지니어',
     description: `안녕하세요! 저는 전문 리뷰어입니다.
+      영화, 책, 음악, 레스토랑, 여행지 등을 평가하며,
+      공정하고 중립적인 리뷰를 작성합니다.
+      늘 사용자들이 신뢰할 수 있는 정보를 제공하는 것을 목표로 하고 있습니다.
+      영화, 책, 음악, 레스토랑, 여행지 등을 평가하며,
+      공정하고 중립적인 리뷰를 작성합니다.
+      늘 사용자들이 신뢰할 수 있는 정보를 제공하는 것을 목표로 하고 있습니다.
+      영화, 책, 음악, 레스토랑, 여행지 등을 평가하며,
+      공정하고 중립적인 리뷰를 작성합니다.
+      늘 사용자들이 신뢰할 수 있는 정보를 제공하는 것을 목표로 하고 있습니다.
       영화, 책, 음악, 레스토랑, 여행지 등을 평가하며,
       공정하고 중립적인 리뷰를 작성합니다.
       늘 사용자들이 신뢰할 수 있는 정보를 제공하는 것을 목표로 하고 있습니다.`,
@@ -21,6 +31,7 @@ export const SocialProfileCard = () => {
 interface SocialProfileCardViewProps {
   profileImage: string;
   nickName: string;
+  carrer: string;
   description: string;
   reviewCount: number;
   follwerCount: number;
@@ -30,6 +41,7 @@ interface SocialProfileCardViewProps {
 export const SocialProfileCardView = ({
   profileImage,
   nickName,
+  carrer,
   description,
   reviewCount,
   follwerCount,
@@ -39,23 +51,25 @@ export const SocialProfileCardView = ({
     <S.ProfileCard>
       <S.ProfileCardContent>
         <S.ProfileDetail>
-          {/* <div
+          <div
             style={{
-              width: '80px',
+              minWidth: '80px',
               height: '80px',
               borderRadius: '50%',
+              overflow: 'hidden',
               backgroundColor: 'white',
-              border: '1px solid white',
             }}
-          > */}
-          <Image
+          ></div>
+          {/* <Image
             width={80}
             height={80}
             src={profileImage}
             alt={'socialProfileImage'}
-          />
-          {/* </div> */}
-          <S.SocialNickname>{nickName}</S.SocialNickname>
+          /> */}
+          <S.FlexItem>
+            <S.SocialNickname>{nickName}</S.SocialNickname>
+            <S.Carrer>{carrer}</S.Carrer>
+          </S.FlexItem>
           <S.Description>{description}</S.Description>
         </S.ProfileDetail>
         <S.AccountDetail>
@@ -93,16 +107,17 @@ const S = {
 
     overflow: hidden;
     color: white;
-
     background-color: ${({ theme }) => theme.colors.gray_7};
   `,
+  FlexItem: styled.div``,
   ProfileCardContent: styled.div`
     padding: 40px 53px;
-    display: flex;
     gap: 60px;
   `,
   ProfileDetail: styled.div`
+    display: flex;
     width: 726px;
+    gap: 20px;
   `,
   AccountDetail: styled.div`
     width: 296px;
@@ -113,7 +128,19 @@ const S = {
     color: white;
     margin: 10px 0;
   `,
+  Carrer: styled.p`
+    font-weight: 400;
+    font-size: 14px;
+  `,
   Description: styled.p`
+    // overflow text ellipsis
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    word-break: break-word;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+
     font-weight: 400;
     font-size: 14px;
     line-height: 140%;
@@ -125,7 +152,7 @@ const S = {
   FollowBox: styled.ul`
     display: flex;
     gap: 24px;
-    margin-bottom: 35px;
+    margin: 16px 0px 40px 0px;
   `,
   FollowBoxItem: styled.li`
     font-style: normal;
