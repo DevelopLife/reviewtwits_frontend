ee22a91ebc9aa29e588980202bd66fff06b87be5
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-13 01:01:35 +0900

    #130 [FEAT] 존재하지 않는 계정의 피드에 접근하는 경우 redirect 기능 추가

--- hooks/useGetSocialProfile.tsx
+++ hooks/useGetSocialProfile.tsx
@@ -1,11 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+
 import { snsAPI } from 'api/sns';
+import { ResponseError } from 'typings/error';
+import { redirectErrorHandler } from 'utils/errorHandler';
 
 const useGetSocialProfile = (nickname: string) => {
   const socialProfileQuery = useQuery(
-    ['socialProfile'],
+    ['socialProfile', nickname],
     () => snsAPI.getProfile(nickname),
     {
+      onError: (err: AxiosError<ResponseError>) => {
+        redirectErrorHandler(err);
+      },
       enabled: !!nickname,
     }
   );
