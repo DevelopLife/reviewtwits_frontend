4276e6863bf5ade749cde0b52d8c2ab10ab3ef7a
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-13 01:02:53 +0900

    #130 [FEAT] 존재하지 않는 계정의 리뷰에 접근하는 경우 redirect 기능 추가

--- hooks/useGetSocialReviews.tsx
+++ hooks/useGetSocialReviews.tsx
@@ -1,11 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 import { snsAPI } from 'api/sns';
+import { ResponseError } from 'typings/error';
+import { redirectErrorHandler } from 'utils/errorHandler';
 
 const useGetSocialReviews = (nickname: string) => {
-  return useQuery(['socialMyReviews'], () => snsAPI.getMyReviews(nickname), {
-    enabled: !!nickname,
-  });
+  return useQuery(
+    ['socialMyReviews', nickname],
+    () => snsAPI.getMyReviews(nickname),
+    {
+      onError: (err: AxiosError<ResponseError>) => redirectErrorHandler(err),
+      enabled: !!nickname,
+    }
+  );
 };
 
 export default useGetSocialReviews;
