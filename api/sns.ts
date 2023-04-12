e1716b815c124118e4cb731d9cd1b444f367c792
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-04-12 22:03:19 +0900

    [FEAT] FollowerList, FloowingList를 받아오는 api 생성

--- api/sns.ts
+++ api/sns.ts
@@ -6,6 +6,15 @@ export const snsAPI = {
   createReview: async (formData: FormData) => {
     return await api.post(`${SNS_URL}/reviews`, formData);
   },
+
+  getFollowerList: async (accountId: string) => {
+    return await api.get(`${SNS_URL}/get-followers/${accountId}`);
+  },
+
+  getFollowingList: async (accountId: string) => {
+    return await api.get(`${SNS_URL}/get-followings/${accountId}`);
+  },
+
   follow: (body: FollowAndUnFollowRequestBody) =>
     api.post(`${SNS_URL}/request-follow`, body),
 
