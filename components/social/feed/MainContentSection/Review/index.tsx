0266523bae62c95c975d4792bd100c5c930b2e30
Jisu <62230430+Jisu00@users.noreply.github.com>
2023-04-13 20:14:40 +0900

    [FEATURE] 피드 페이지 리뷰 구현 (#128)
    * [FIX] React import 에러 해결
    
    * [FIX] formattedImageUrl util 함수 null 반환하지 않도록 수정
    
    * [ADD] SNS 피드 페이지 시멘틱 마크업 및 컴포넌트 분리 완료
    
    * [ADD] 페이지에 Social Layout 적용
    
    * #92 [ADD] 이모지 svg 파일 추가 및 이모지 -> svg로 설정
    
    * #92 [FEAT] SNS 피드 퍼블리싱
    
    * [ADD] UserProfileType -> UserProfileFormType으로 타입 이름 수정
    
    * [ADD] 반응 이모지 전체 PNG 파일 추가
    
    * #92 [FEAT] MainContentSection 서버 데이터 렌더링 완료
    
    * [REFACTOR] Review 내부 컴포넌트 분리
    
    * #92 [FEAT] 반응 추가 기능 구현
    
    * #92 [FEAT] 이미 선택된 반응 삭제 기능 구현
    
    * #92 [FEAT] 스크랩 기능 구현
    
    * #92 [ADD] 디자인 변경 사항 적용
    
    * #92 [FEAT] 리액션 추가 List 및 useReaction 구현
    
    * #92 [FEAT] '상품 구매' 버튼 클릭 시 상품 URL로 연결
    
    * #92 [FEAT] '상품 정보' 버튼 클릭 시 상품 정보 페이지로 이동 (임시)

---
+++ components/social/feed/MainContentSection/Review/index.tsx
@@ -0,0 +1,204 @@
+import Image from 'next/image';
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+
+import { Colors } from 'styles/theme';
+
+import { ReviewResponseType } from 'typings/reviews';
+import { formattedLastTime } from 'utils/format';
+
+import Card from '../../Card';
+import ReactionBox from './ReactionBox';
+import StarBox from './StarBox';
+import ImageList from './ImageList';
+import AddReactionBox from './AddReactionBox';
+import ScrapButton from './ScrapButton';
+import CommentIcon from 'public/icons/comment.svg';
+
+interface ReviewProps {
+  data?: ReviewResponseType;
+}
+
+const Review = ({ data }: ReviewProps) => {
+  const router = useRouter();
+
+  const goProductPage = () => data && router.push(data.productUrl);
+
+  // 임시
+  const goProductInfoPage = () =>
+    data && router.push(`/product?name=${data.productName}`);
+
+  const props = {
+    data,
+    isReactionExist:
+      data?.reactionResponses && Object.keys(data.reactionResponses).length > 0,
+    goProductPage,
+    goProductInfoPage,
+  };
+
+  return <ReviewView {...props} />;
+};
+
+interface ReviewViewProps {
+  data?: ReviewResponseType;
+  isReactionExist?: boolean;
+  goProductPage: () => void;
+  goProductInfoPage: () => void;
+}
+
+const ReviewView = ({
+  data,
+  isReactionExist,
+  goProductPage,
+  goProductInfoPage,
+}: ReviewViewProps) => {
+  if (!data) return null;
+  return (
+    <Card>
+      <S.Content>
+        <S.ScrapButtonWrap>
+          <ScrapButton isScrapped={data.isScrapped} reviewId={data.reviewId} />
+        </S.ScrapButtonWrap>
+        <S.ReviewInfoBox>
+          <S.UserInfo>
+            <S.UserImage src="" alt="" />
+            <S.Nickname>{data?.userInfo?.nickname}</S.Nickname>
+          </S.UserInfo>
+          <StarBox score={data?.score} />
+          <S.LastTime>{formattedLastTime(data?.lastModifiedDate)}</S.LastTime>
+        </S.ReviewInfoBox>
+        <S.ReviewText>{data?.content}</S.ReviewText>
+        <ImageList imageNameList={data?.reviewImageNameList} />
+        <S.UserResponseBox>
+          <AddReactionBox reviewId={data?.reviewId} />
+          <S.CommentButton>
+            <CommentIcon />
+            {data?.commentCount}
+          </S.CommentButton>
+        </S.UserResponseBox>
+        {isReactionExist && (
+          <ReactionBox
+            reviewId={data?.reviewId}
+            reactions={data?.reactionResponses}
+          />
+        )}
+        <S.ButtonBox>
+          <S.Button color="primary" onClick={goProductPage}>
+            상품 구매
+          </S.Button>
+          <S.Button color="secondary" onClick={goProductInfoPage}>
+            상품 정보
+          </S.Button>
+        </S.ButtonBox>
+      </S.Content>
+    </Card>
+  );
+};
+
+export default Review;
+
+const S = {
+  Content: styled.div`
+    display: flex;
+    flex-direction: column;
+    position: relative;
+
+    width: 100%;
+    max-height: 490px;
+    padding: 32px;
+  `,
+
+  ScrapButtonWrap: styled.div`
+    position: absolute;
+    top: 15px;
+    right: 5px;
+  `,
+
+  ReviewInfoBox: styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  `,
+
+  UserInfo: styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  `,
+
+  UserImage: styled(Image)`
+    background: gray;
+    border-radius: 50%;
+    width: 32px;
+    height: 32px;
+  `,
+
+  Nickname: styled.span`
+    font-size: 16px;
+    font-weight: 500;
+    color: black;
+  `,
+
+  LastTime: styled.span`
+    font-size: 12px;
+  `,
+
+  ReviewText: styled.p`
+    display: -webkit-box;
+    height: 100px;
+    overflow: hidden;
+    margin: 24px 0;
+
+    font-size: 14px;
+    line-height: 1.4;
+    word-break: keep-all;
+    white-space: normal;
+    -webkit-line-clamp: 5;
+    -webkit-box-orient: vertical;
+  `,
+
+  UserResponseBox: styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+
+    position: relative;
+    margin-top: 24px;
+  `,
+
+  CommentButton: styled.button`
+    display: flex;
+    justify-content: center;
+    gap: 5px;
+
+    color: black;
+    padding: 0 0 2px 0;
+    font-size: 18px;
+    font-weight: 700;
+
+    width: fit-content;
+  `,
+
+  ButtonBox: styled.div`
+    display: flex;
+    gap: 16px;
+
+    margin-top: 40px;
+  `,
+
+  Button: styled.button<{ color: Colors }>`
+    background: ${({ color, theme }) => theme.colors[color]};
+    font-size: 18px;
+    padding: 17px;
+    color: white;
+    border-radius: 15px;
+
+    :first-child {
+      width: 70%;
+    }
+
+    :last-child {
+      width: 30%;
+    }
+  `,
+};
