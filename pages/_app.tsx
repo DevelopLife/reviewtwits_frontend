7f2133816b0482d229f41e3636bb3548992e0af9
Jisu00 <jisudev00@gmail.com>
2023-04-14 17:27:27 +0900

    #127 [FEAT] 접속 디바이스 판별 구현

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,15 +7,22 @@ import {
 import { Global, ThemeProvider } from '@emotion/react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import reset from 'styles/reset';
 import theme from 'styles/theme';
 import ModalContainer from 'components/common/Modal/ModalContainer';
 
+import { validateMobile } from 'utils/validate';
+
 export default function App({ Component, pageProps }: AppProps) {
   // TODO: consider setting defaultOptions
   const [queryClient] = useState(() => new QueryClient());
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const isMobile = validateMobile(userAgent);
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
