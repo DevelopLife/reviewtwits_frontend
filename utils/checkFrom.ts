b03d7dab4a4fc9280f7b5a4aa5caa4cfa001a385
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-14 21:29:40 +0900

    [TEST] referrer에 따라 boolean을 반환하는 함수의 Test code 작성

--- utils/checkFrom.ts
+++ utils/checkFrom.ts
@@ -1,6 +1,10 @@
 export function checkPreviousHostnameEqualMyHostName() {
   const previousFrom = document?.referrer;
-  const previousHostname = new URL(previousFrom)?.hostname;
 
-  return previousHostname === 'reviewtwits.shop';
+  if (previousFrom) {
+    const previousHostname = new URL(previousFrom)?.hostname;
+    return previousHostname === 'reviewtwits.shop';
+  }
+
+  return true;
 }
