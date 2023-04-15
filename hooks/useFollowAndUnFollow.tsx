2228eedc948df43be28a793f0322781a87e668be
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-16 01:12:10 +0900

    [FEAT] follow, unfollow 시 following query 무효화

--- hooks/useFollowAndUnFollow.tsx
+++ hooks/useFollowAndUnFollow.tsx
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 
 import { snsAPI } from 'api/sns';
@@ -6,15 +6,25 @@ import { alertErrorHandler } from 'utils/errorHandler';
 import type { ResponseError } from 'typings/error';
 
 const useFollowAndUnFollow = (targetUserAccountId: string) => {
+  const queryClient = useQueryClient();
+  const invalidateUserGetFollowingListQuery = () =>
+    queryClient.invalidateQueries(['useGetFollowingList']);
+
   const { mutate: follow } = useMutation(
     () => snsAPI.follow({ targetUserAccountId }),
     {
+      onSuccess: () => {
+        invalidateUserGetFollowingListQuery();
+      },
       onError: (err: AxiosError<ResponseError>) => alertErrorHandler(err),
     }
   );
   const { mutate: unfollow } = useMutation(
     () => snsAPI.unfollow({ targetUserAccountId }),
     {
+      onSuccess: () => {
+        invalidateUserGetFollowingListQuery();
+      },
       onError: (err: AxiosError<ResponseError>) => alertErrorHandler(err),
     }
   );
