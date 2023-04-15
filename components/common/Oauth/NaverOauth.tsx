c8d1d12d9c6fb6aa195b151a41de20f14a725066
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-15 19:21:36 +0900

    #133 [FIX] Next.js Image 최적화시 naverlogin_sdk 에러 해결

--- components/common/Oauth/NaverOauth.tsx
+++ components/common/Oauth/NaverOauth.tsx
@@ -38,7 +38,7 @@ export const NaverSDKScript = () => {
   return (
     <script
       type="text/javascript"
-      src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js"
+      src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js"
       async
     ></script>
   );
