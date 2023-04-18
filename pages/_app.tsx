ada88e5ffc7fbd7909bd60af899f0086e3189e0e
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-18 19:15:36 +0900

    [FEAT] _app.tsx 파일에 GlobalProvider 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,17 @@
 import type { AppProps } from 'next/app';
-import {
-  QueryClientProvider,
-  QueryClient,
-  Hydrate,
-} from '@tanstack/react-query';
-import { Global, ThemeProvider } from '@emotion/react';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RecoilRoot } from 'recoil';
-import { useState } from 'react';
+import type { DehydratedState } from '@tanstack/react-query';
 
-import reset from 'styles/reset';
-import theme from 'styles/theme';
 import ModalContainer from 'components/common/Modal/ModalContainer';
+import GlobalProvider from 'components/common/GlobalProvider';
 
-export default function App({ Component, pageProps }: AppProps) {
-  // TODO: consider setting defaultOptions
-  const [queryClient] = useState(() => new QueryClient());
-
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Global styles={reset} />
-      <ReactQueryDevtools initialIsOpen={false} />
-      <Hydrate state={pageProps.dehydratedState}>
-        <ThemeProvider theme={theme}>
-          <RecoilRoot>
-            <ModalContainer />
-            <Component {...pageProps} />
-          </RecoilRoot>
-        </ThemeProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <GlobalProvider pageProps={pageProps}>
+      <ModalContainer />
+      <Component {...pageProps} />
+    </GlobalProvider>
   );
 }
