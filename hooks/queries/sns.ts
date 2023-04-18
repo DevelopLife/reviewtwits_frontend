2fa749ec515d422eebd54f362a50019995f37eb7
YeongMin Ahn <ahn0min95@gmail.com>
2023-04-19 03:27:51 +0900

    [ADD] 팔로우 리스트의 변형인 nickname을 key값으로 가지는 딕셔너리 쿼리 추가

--- hooks/queries/sns.ts
+++ hooks/queries/sns.ts
@@ -1,8 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { snsAPI } from 'api/sns';
 import { AxiosError, AxiosResponse } from 'axios';
 import { ResponseError } from 'typings/error';
-import { FollowListType } from 'typings/sns';
+import { FollowListType, FollowingDictionary } from 'typings/sns';
 
 export const useGetFollowerList = (accountId: string) => {
   return useQuery<AxiosResponse<FollowListType>, AxiosError<ResponseError>>(
@@ -12,8 +12,39 @@ export const useGetFollowerList = (accountId: string) => {
 };
 
 export const useGetFollowingList = (accountId: string) => {
+  const queryClient = useQueryClient();
+
+  const setFollowingDictionary = (followingList: FollowListType) => {
+    const defaultFollowingDictionary: FollowingDictionary = {};
+
+    const result = followingList.reduce(
+      (followingDictionary, { nickname, ...rest }) => {
+        followingDictionary[nickname] = rest;
+        return followingDictionary;
+      },
+      defaultFollowingDictionary
+    );
+
+    return result;
+  };
+
   return useQuery<AxiosResponse<FollowListType>, AxiosError>(
     ['useGetFollowingList'],
-    () => snsAPI.getFollowingList(accountId)
+    () => snsAPI.getFollowingList(accountId),
+    {
+      onSuccess: (response) => {
+        queryClient.cancelQueries(['isFollowingDictionary']);
+        queryClient.fetchQuery({
+          queryKey: ['isFollowingDictionary'],
+          queryFn: () => setFollowingDictionary(response.data),
+        });
+      },
+    }
+  );
+};
+
+export const useGetMyReviews = (nickname: string, reviewId?: number) => {
+  return useQuery(['reviews', nickname], () =>
+    snsAPI.getMyReviews(nickname, reviewId)
   );
 };
