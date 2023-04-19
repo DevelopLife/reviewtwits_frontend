32812aced8b45e6708639e64c524b6f613ae6ba6
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-04-19 09:12:49 +0900

    [ADD] infiniteScroll 설명을 위한 reactQuery 생성

--- hooks/queries/sns.ts
+++ hooks/queries/sns.ts
@@ -1,19 +1,31 @@
 import { useQuery } from '@tanstack/react-query';
 import { snsAPI } from 'api/sns';
 import { AxiosError, AxiosResponse } from 'axios';
+import useInfiniteScrollQuery from 'hooks/queries/useInfiniteScrollQuery';
+import { ResponseError } from 'typings/error';
+import { ReviewResponseType } from 'typings/reviews';
 import { FollowListType } from 'typings/sns';
 
 export const useGetFollowerList = (accountId: string) => {
-return useQuery<
-    AxiosResponse<FollowListType>,
-    AxiosError<ResponseError>
-  >(['useGetFollowerList'], () => snsAPI.getFollowerList(accountId));
+  return useQuery<AxiosResponse<FollowListType>, AxiosError<ResponseError>>(
+    ['useGetFollowerList'],
+    () => snsAPI.getFollowerList(accountId)
+  );
 };
 
 export const useGetFollowingList = (accountId: string) => {
-  const { data, isLoading } = useQuery<AxiosResponse, AxiosError>(
+  return useQuery<AxiosResponse, AxiosError<ResponseError>>(
     ['useGetFollowingList'],
     () => snsAPI.getFollowingList(accountId)
   );
-  return { isLoading, data };
+};
+
+// infiniteScroll 설명을 위한 임시 작업
+export const useGetInfiniteFeed = () => {
+  return useInfiniteScrollQuery({
+    queryKey: ['useGetInfiniteFeed'],
+    getNextPage: (nextRequest) => {
+      return snsAPI.getInfiniteFeed(nextRequest);
+    },
+  });
 };
