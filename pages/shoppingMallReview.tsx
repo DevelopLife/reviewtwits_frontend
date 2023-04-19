58ed41d033d19ddf3c4b2d4e11a9bfbb51ab432c
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-04-19 16:00:27 +0900

    [FEAT] ShoppingMallReview 페이지에서 api 결과값 받아오기
    여러 컴포넌트에서 데이터를 각자 받아오는것보다 페이지에서 받아오는게
    페이지 역할에 대한 이해를 더 빠르게 할 수 있을거라고 생각하고 작업.

--- pages/shoppingMallReview.tsx
+++ pages/shoppingMallReview.tsx
@@ -1,10 +1,17 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import ShoppingMallReview from 'components/ShoppingMallReview/ShoppingMallReview';
-import React from 'react';
+import { useGetShoppingMallReviewInfo } from 'hooks/queries/reviews';
 
 const shoppingMallReview = () => {
+  const { data: shoppingmallReviewInfoData } = useGetShoppingMallReviewInfo();
+
+  if (!shoppingmallReviewInfoData) return <h1>데이터가 없습니다</h1>;
+
   return (
     <div>
-      <ShoppingMallReview />
+      <ShoppingMallReview
+        shoppingmallReviewInfoData={shoppingmallReviewInfoData?.data}
+      />
     </div>
   );
 };
