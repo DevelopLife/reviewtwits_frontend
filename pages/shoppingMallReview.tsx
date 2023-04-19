797edc99e3d29b261b3247210d8a8890d6ea7002
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-04-19 18:23:38 +0900

    [FEAT] shoppingMallReview 페이지에서 데이터 받아오기

--- pages/shoppingMallReview.tsx
+++ pages/shoppingMallReview.tsx
@@ -1,16 +1,21 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import ShoppingMallReview from 'components/ShoppingMallReview/ShoppingMallReview';
-import { useGetShoppingMallReviewInfo } from 'hooks/queries/reviews';
+import {
+  useGetShoppingMallReviewInfo,
+  useGetShoppingMallReviewList,
+} from 'hooks/queries/reviews';
 
 const shoppingMallReview = () => {
   const { data: shoppingmallReviewInfoData } = useGetShoppingMallReviewInfo();
-
-  if (!shoppingmallReviewInfoData) return <h1>데이터가 없습니다</h1>;
+  const { data: shoppingmallReviewList } = useGetShoppingMallReviewList();
+  if (!shoppingmallReviewInfoData || !shoppingmallReviewList)
+    return <h1>데이터가 없습니다</h1>;
 
   return (
     <div>
       <ShoppingMallReview
         shoppingmallReviewInfoData={shoppingmallReviewInfoData?.data}
+        shoppingmallReviewList={shoppingmallReviewList?.data}
       />
     </div>
   );
