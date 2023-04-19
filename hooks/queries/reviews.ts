3aa0384820534d0e26c433c40390962d2062a7d0
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-04-19 15:56:44 +0900

    [FEAT] 쇼핑몰 리뷰 전체 정보를 받아와 react query에 저장하는 hook 제작

---
+++ hooks/queries/reviews.ts
@@ -0,0 +1,14 @@
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
+import { shoppingAPI } from 'api/reviews';
+import { AxiosError, AxiosResponse } from 'axios';
+import { ResponseError } from 'typings/error';
+import { ShoppingMallReviewInfo } from 'typings/reviews';
+
+export const useGetShoppingMallReviewInfo = () => {
+  return useQuery<
+    AxiosResponse<ShoppingMallReviewInfo>,
+    AxiosError<ResponseError>
+  >(['useGetShoppingMallReviewInfo'], () =>
+    shoppingAPI.getShoppingMallReviewInfo()
+  );
+};
