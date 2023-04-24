152c03d190e64431000eaf653bb9b18a4665a710
Jisu00 <jisudev00@gmail.com>
2023-04-24 18:45:18 +0900

    #151 [FEAT] 로그인 시, 유저 이미지 렌더링

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -4,10 +4,12 @@ import { useState } from 'react';
 
 import * as S from './Header.styles';
 import UserMenu from './UserMenu';
+import useUserProfile from 'hooks/useUserProfile';
+import { formattedImageUrl } from 'utils/format';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-  const [userLoggedIn] = useState<boolean>(false);
+  const { isLogin, profileImageUrl } = useUserProfile();
 
   const toggleMenuOpen = () => setIsMenuOpen((prev) => !prev);
   const closeMenu = () => setIsMenuOpen(false);
@@ -23,14 +25,16 @@ const Header = () => {
           <S.NavLink>Docs</S.NavLink>
         </S.Navbar>
         <S.RightBox>
-          {!userLoggedIn ? (
+          {isLogin ? (
             <>
               <S.Profile onClick={toggleMenuOpen}>
                 <Image
-                  src="https://s3-alpha-sig.figma.com/img/bb12/da5f/d8677bd88607c96432496808e0e191d2?Expires=1678060800&amp;Signature=Rkde6MRIbKJc3j5My4a6xUvnZPe9GhqxjXjMJOk9iV4-CHGuFnPI-hXnSig4WyhZGHbSELzra2MT6u0rMyiXM48MggRqcCq35zyFWkl3P8rMnN~95c7mIEXXZNacE7QYXT3OWgY~nhLTW4MbpivikVUMhhS-Un4Ut2kiAPDQbG-wFIuClZs3b-v1oRdMzFvGPU9J2~8PiSbFWBDf0svozdV6oE16npwK617OjPwQDxscqVQLeDI2FT-uYm5EV7i6MSR59pQgrFYRsAUNJm5DRYu8CIzUB0U-~yz9TQjxb-rKYEQLCkqWIaQEQLhsdrXXQMjKy6QKIv9oqUryjnwVtA__&amp;Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                  src={
+                    profileImageUrl ? formattedImageUrl(profileImageUrl) : ''
+                  }
                   width={40}
                   height={40}
-                  alt={''}
+                  alt="userImg"
                 />
               </S.Profile>
               {isMenuOpen && <UserMenu closeMenu={closeMenu} />}
--- hooks/useUserProfile.tsx
+++ hooks/useUserProfile.tsx
@@ -2,15 +2,31 @@ import { useQuery } from '@tanstack/react-query';
 import { usersAPI } from 'api/users';
 import { UserProfileResponseType } from 'typings/account';
 
+import { isLoginState } from 'states/isLogin';
+import { useRecoilValue } from 'recoil';
+
 const USER_PROFILE_QUERY = 'userProfile';
 
 const useUserProfile = () => {
+  const isLogin = useRecoilValue(isLoginState);
   const { data: userData } = useQuery<UserProfileResponseType>(
-    [USER_PROFILE_QUERY],
-    usersAPI.getUserProfile
+    [USER_PROFILE_QUERY, isLogin],
+    usersAPI.getUserProfile,
+    {
+      enabled: !!isLogin,
+    }
   );
 
-  return userData;
+  return {
+    isLogin,
+    profileImageUrl: userData?.profileImageUrl,
+    nickname: userData?.nickname,
+    introduceText: userData?.introduceText,
+    accountId: userData?.accountId,
+    birthDate: userData?.birthDate,
+    gender: userData?.gender,
+    phoneNumber: userData?.phoneNumber,
+  };
 };
 
 export default useUserProfile;
