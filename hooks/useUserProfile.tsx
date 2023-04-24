f9ffb979b65f8d5b11e4f56484c4aa7918702521
Jisu00 <jisudev00@gmail.com>
2023-04-24 17:43:33 +0900

    [FEAT] 프로필 수정 컴포넌트 수정된 API 응답 대응

--- hooks/useUserProfile.tsx
+++ hooks/useUserProfile.tsx
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { usersAPI } from 'api/users';
+import { UserProfileResponseType } from 'typings/account';
 
 const USER_PROFILE_QUERY = 'userProfile';
 
 const useUserProfile = () => {
-  const { data: userData } = useQuery(
+  const { data: userData } = useQuery<UserProfileResponseType>(
     [USER_PROFILE_QUERY],
     usersAPI.getUserProfile
   );
