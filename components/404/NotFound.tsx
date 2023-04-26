5486fc2068774fca9cfd1fb615dd34fd9b595011
Jisu <62230430+Jisu00@users.noreply.github.com>
2023-04-26 11:59:55 +0900

    #162 [FEAT] 404 페이지 구현 (#165)

---
+++ components/404/NotFound.tsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+import Button from 'components/common/Button';
+import Image from 'next/image';
+
+const NotFound = () => {
+  return (
+    <S.Container>
+      <S.Box>
+        <Image
+          width={676}
+          height={676}
+          quality={100}
+          src="/images/not_found_img.png"
+          alt="notFoundImg"
+        />
+        <S.Content>
+          <h1>해당 페이지를 찾지 못했습니다</h1>
+          <small>주소가 잘못되었거나 더 이상 제공되지 않는 페이지입니다</small>
+
+          <Button color="primary" large>
+            메인페이지로 이동
+          </Button>
+        </S.Content>
+      </S.Box>
+    </S.Container>
+  );
+};
+
+export default NotFound;
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 100vh;
+  `,
+
+  Box: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 100px;
+  `,
+
+  Content: styled.div`
+    display: flex;
+    flex-direction: column;
+
+    white-space: pre;
+
+    h1 {
+      font-size: 36px;
+      font-weight: 700;
+    }
+
+    small {
+      font-size: 16px;
+      margin-top: 15px;
+      margin-bottom: 56px;
+    }
+  `,
+};
