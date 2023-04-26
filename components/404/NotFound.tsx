75ca1f38acca3db44dab725f1bc8507af41ffe46
Jisu <62230430+Jisu00@users.noreply.github.com>
2023-04-26 12:11:14 +0900

    [FEATURE] 404 페이지 내부 버튼 링크 연결 (#167)
    * #162 [FEAT] 404 페이지 구현
    
    * #162 [FEAT] 메인페이지 이동 Link 추가

--- components/404/NotFound.tsx
+++ components/404/NotFound.tsx
@@ -1,6 +1,10 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+
 import styled from '@emotion/styled';
 import Button from 'components/common/Button';
-import Image from 'next/image';
+
 
 const NotFound = () => {
   return (
@@ -16,10 +20,11 @@ const NotFound = () => {
         <S.Content>
           <h1>해당 페이지를 찾지 못했습니다</h1>
           <small>주소가 잘못되었거나 더 이상 제공되지 않는 페이지입니다</small>
-
-          <Button color="primary" large>
-            메인페이지로 이동
-          </Button>
+          <Link href="/">
+            <Button color="primary" large>
+              메인페이지로 이동
+            </Button>
+          </Link>
         </S.Content>
       </S.Box>
     </S.Container>
