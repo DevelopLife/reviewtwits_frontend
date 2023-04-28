30fb85b0d14334dd646b85e6db30aacc091aae72
Jisu00 <jisudev00@gmail.com>
2023-04-28 13:17:26 +0900

    [ADD] 유저 통계 요청 컴포넌트 분리

---
+++ components/Statistics/ReportUserStatistics.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+
+import { getCookie, setCookie } from 'utils/cookies';
+import { validateMobile } from 'utils/validate';
+
+const ReportUserStatistics = () => {
+  const getVisitorInfo = () => {
+    const userAgent = navigator.userAgent;
+    const device = validateMobile(userAgent) ? 'MOBILE' : 'PC';
+    const searchRoute = document.referrer;
+
+    return { device, searchRoute };
+  };
+
+  useEffect(() => {
+    const isVisited = getCookie('visited');
+
+    if (isVisited === 'Y') return;
+
+    const { device, searchRoute } = getVisitorInfo();
+
+    // Request
+
+    setCookie('visited', 'Y');
+  }, []);
+
+  return <></>;
+};
+
+export default ReportUserStatistics;
