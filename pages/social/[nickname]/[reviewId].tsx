223f6c270d0dce92e569449716145e322409684b
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-04-29 22:04:50 +0900

    [FEAT] 리뷰 모달 팝업 페이지 제작

---
+++ pages/social/[nickname]/[reviewId].tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import SNSExplorePage from '../home/index';
+
+const ReviewModalPage = () => {
+  return (
+    <>
+      <div>
+        <h1>배경배경</h1>
+        <div>바보</div>
+      </div>
+      <div>
+        {/* 임시로 mainpage에서만 되게 작업 */}
+        <SNSExplorePage />
+        {/* <UserPage /> */}
+        {/* <SocialUserFeedPage /> */}
+      </div>
+    </>
+  );
+};
+
+export default ReviewModalPage;
