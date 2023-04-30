3e80a661ff1ac24abaead21ab7600562c0dcbf4c
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-04-30 18:17:47 +0900

    [FEAT] 모달 이미지 페이지네이션
    보고 싶은 이미지 선택 가능
    backward, forward 화살표를 통한 이미지 선택 가능

--- components/common/Modal/SocialFeedModal/DetailImage.tsx
+++ components/common/Modal/SocialFeedModal/DetailImage.tsx
@@ -1,33 +1,69 @@
 import styled from '@emotion/styled';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { formattedImageUrl } from 'utils/format';
+
+import Backward from 'public/icons/backward.svg';
+import Forward from 'public/icons/forward.svg';
 
 interface DetailImageProps {
   reviewImageUrlList: string[];
 }
 
 const DetailImage = ({ reviewImageUrlList }: DetailImageProps) => {
+  const [selectedImageIdx, setSelectedImageIdx] = useState<number>(0);
+
+  const onImageClick = (idx: number) => {
+    setSelectedImageIdx((prev) => idx);
+  };
+
+  const onSelectNextImage = () => {
+    if (reviewImageUrlList.length <= selectedImageIdx + 1) {
+      setSelectedImageIdx((prev) => 0);
+      return;
+    }
+    setSelectedImageIdx((prev) => prev + 1);
+  };
+
+  const onSelectBeforeImage = () => {
+    if (0 >= selectedImageIdx - 1) {
+      setSelectedImageIdx((prev) => reviewImageUrlList.length - 1);
+      return;
+    }
+    setSelectedImageIdx((prev) => prev - 1);
+  };
   return (
     <S.Container>
-      <S.LeftButton>left</S.LeftButton>
-      <div>
-        {reviewImageUrlList.map((reviewImageUrl) => (
+      <S.Backward onClick={onSelectBeforeImage}>
+        <Backward />
+      </S.Backward>
+      <S.ImageContainer>
+        <S.SelectedImageBox>
           <Image
-            key={reviewImageUrl}
-            src={formattedImageUrl(reviewImageUrl)}
+            src={formattedImageUrl(reviewImageUrlList[selectedImageIdx])}
             alt=""
             width={568}
             height={603}
           />
-        ))}
+        </S.SelectedImageBox>
+
         <S.ImagePaginators>
-          <S.ImagePaginator target={true} />
-          <S.ImagePaginator />
-          <S.ImagePaginator />
+          {reviewImageUrlList.map((reviewImageUrl, idx) => (
+            <S.ImagePaginator
+              key={reviewImageUrl}
+              src={formattedImageUrl(reviewImageUrl)}
+              alt=""
+              width={160}
+              height={160}
+              onClick={() => onImageClick(idx)}
+              isActiveImage={selectedImageIdx === idx}
+            />
+          ))}
         </S.ImagePaginators>
-      </div>
-      <S.RightButton>right</S.RightButton>
+      </S.ImageContainer>
+      <S.Forward onClick={onSelectNextImage}>
+        <Forward />
+      </S.Forward>
     </S.Container>
   );
 };
@@ -37,36 +73,58 @@ const S = {
     display: flex;
     flex-direction: row;
     justify-content: center;
+    gap: 16px;
 
     width: 744px;
 
-    margin-top: 48px;
-    margin-bottom: 48px;
+    padding-top: 48px;
+    padding-bottom: 48px;
 
-    border: 1px solid pink;
+    background: #181818;
+    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.21);
+    border-radius: 20px 0px 0px 20px;
   `,
 
-  LeftButton: styled.button``,
-  RightButton: styled.button``,
+  ImageContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `,
 
+  Backward: styled.button`
+    color: white;
+  `,
+  Forward: styled.button`
+    color: white;
+  `,
+
+  SelectedImageBox: styled.div`
+    width: 568px;
+    height: 603px;
+    overflow: hidden;
+  `,
   ImagePaginators: styled.ul`
     display: flex;
     flex-direction: row;
-    gap: 8px;
+    gap: 16px;
 
     align-items: center;
-    justify-content: center;
+    justify-content: left;
 
-    max-width: 654px;
-    margin-top: 21px;
-    height: 12px;
+    width: 512px;
+    height: 160px;
+    margin-top: 40px;
+
+    /* overflow-x: scroll; */
   `,
-  ImagePaginator: styled.li<{ target?: boolean }>`
-    width: 12px;
-    height: 12px;
-    background-color: ${({ target, theme }) =>
-      target ? theme.colors.secondary : '#d9d9d9'};
-    border-radius: 50%;
+  ImagePaginator: styled(Image)<{ isActiveImage: boolean }>`
+    width: 160px;
+    height: 160px;
+
+    opacity: ${({ isActiveImage }) => (isActiveImage ? '0.9' : '0.5')};
+
+    border-radius: 26px;
   `,
 };
 
