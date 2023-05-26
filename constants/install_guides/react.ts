baebb3f44c6f04cd2fa5e1e153643dcf768ba070
YeongMin Ahn <ahn0min95@gmail.com>
2023-05-26 12:35:34 +0900

    [FEAT] 리액트용 ReviewTwits 필수 패키지 설치 코드 변경

--- constants/install_guides/react.ts
+++ constants/install_guides/react.ts
@@ -10,22 +10,22 @@ export const REACT_INSTALL_GUIDES: InstallGuild[] = [
   {
     TEXT: '1. 리액트용 ReviewTwits 필수 패키지를 설치합니다.',
     CODE: `
-      npm install @review-twits/react
-      
-      yarn add @review-twits/react
+      $ npm install @review-twits/react
+
+      # or
+
+      $ yarn add @review-twits/react
       `,
   },
   {
     TEXT: '2. 리뷰리스트 부착을 위한 코드 예시입니다.',
     CODE: `
-      import {ReviewIframe} from "@review-twits/react";
+      import { ReviewIframe } from "@review-twits/react";
     
       const projectName = "${projectname}";
       
       const YourProductPage = () => {
-        return (
-            <ReviewIframe projectName={projectName} />
-        );
+        return <ReviewIframe projectName={projectName} />
       };
       
       export default YourProductPage
@@ -34,12 +34,12 @@ export const REACT_INSTALL_GUIDES: InstallGuild[] = [
   {
     TEXT: '3. 리뷰 작성 팝업창을 열기 위한 코드 예시입니다.',
     CODE: `
-      import {useReviewWrite} from "@review-twits/react";
+      import { useReviewWrite } from "@review-twits/react";
     
       const projectName = "${projectname}";
       
       const YourProductPage = () => {
-        const {openReviewWritePage} = useReviewWrite();
+        const { openReviewWritePage } = useReviewWrite();
       
         return (
           <div>
