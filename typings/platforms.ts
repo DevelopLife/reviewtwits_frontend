6c860229d04904eabdb06723da798c12d9f268b1
YeongMin Ahn <ahn0min95@gmail.com>
2023-05-29 23:17:01 +0900

    Chore: install platform 관련 타입 분리 및 적용

---
+++ typings/platforms.ts
@@ -0,0 +1,5 @@
+type Platforms = 'React' | 'html';
+type PlatformLowercases = Lowercase<Platforms>;
+type PlatformUppercases = Uppercase<Platforms>;
+
+export type { Platforms, PlatformLowercases, PlatformUppercases };
