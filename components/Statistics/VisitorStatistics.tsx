086562dfa09761295eeaed191d48526745f2bb70
YeongMin Ahn <ahn0min95@gmail.com>
2023-05-31 23:35:32 +0900

    [FEAT] 방문자 통계 실제 데이터와 연동

--- components/Statistics/VisitorStatistics.tsx
+++ components/Statistics/VisitorStatistics.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import Shadow from './common/Shadow';
 import * as S from './VisitorStatistics.styles';
+import { RecentVisitCounts } from 'typings/statistics';
 
-const VisitorStatistics = () => {
+type VisitorStatisticsProps = Partial<RecentVisitCounts>;
+
+const VisitorStatistics = ({
+  presentVisit = 0,
+  previousVisit = 0,
+  totalVisit = 0,
+}: VisitorStatisticsProps) => {
   return (
     <Shadow boxWidth={682} boxHeight={200}>
       <S.Container>
         <S.VisitorInfoBox>
           <S.VisitorInfo>오늘 방문자수</S.VisitorInfo>
-          <S.VisitorTotalNumber>423</S.VisitorTotalNumber>
+          <S.VisitorTotalNumber>{presentVisit}</S.VisitorTotalNumber>
         </S.VisitorInfoBox>
         <S.VisitorInfoBox>
           <S.VisitorInfo>어제 방문수</S.VisitorInfo>
-          <S.VisitorTotalNumber>412</S.VisitorTotalNumber>
+          <S.VisitorTotalNumber>{previousVisit}</S.VisitorTotalNumber>
         </S.VisitorInfoBox>
         <S.VisitorInfoBox>
           <S.VisitorInfo>누적 방문수</S.VisitorInfo>
           <S.VisitorTotalNumber pointColor="blue_1">
-            579,333
+            {totalVisit}
           </S.VisitorTotalNumber>
         </S.VisitorInfoBox>
       </S.Container>
--- pages/statistics/index.tsx
+++ pages/statistics/index.tsx
@@ -2,27 +2,46 @@ import {
   VisitorStatistics,
   ViewsStatistics,
   LeadTimeRate,
-  ProductStatistics,
+  // ProductStatistics,
 } from 'components/Statistics/@index';
 import Margin from 'components/Statistics/common/Margin';
 import StatisticsPageLayout from 'components/Statistics/common/StatisticsPageLayout';
+import SimpleBarChart from 'components/common/Charts/BarChart';
+import useStatistics from 'hooks/queries/statistics';
 import React from 'react';
 
 const ProjectStatisticsPage = () => {
+  const {
+    recentVisitCountsQuery,
+    dailyVisitGraphInfosQuery,
+    visitGraphInfosQuery,
+  } = useStatistics({
+    projectId: '25020',
+    // range,
+    // interval,
+  });
+
+  const { data: recentVisitCounts } = recentVisitCountsQuery;
+  const { data: dailyVisitGraphInfos } = dailyVisitGraphInfosQuery;
+  const { data: visitGraphInfos } = visitGraphInfosQuery;
+
   return (
     <StatisticsPageLayout>
       <Margin marginTop={60}>
-        <VisitorStatistics />
+        <VisitorStatistics {...recentVisitCounts?.data} />
       </Margin>
       <Margin marginTop={40}>
-        <ViewsStatistics />
+        <ViewsStatistics>
+          <SimpleBarChart data={dailyVisitGraphInfos?.data?.visitInfo} />
+        </ViewsStatistics>
       </Margin>
       <Margin marginTop={48}>
-        <LeadTimeRate />
+        <LeadTimeRate
+          chartDatas={visitGraphInfos?.data}
+          dataKey={'previousVisit'}
+        />
       </Margin>
-      <Margin marginTop={32}>
-        <ProductStatistics />
-      </Margin>
+      <Margin marginTop={32}>{/* <ProductStatistics /> */}</Margin>
       <Margin marginBottom={60} />
     </StatisticsPageLayout>
   );
