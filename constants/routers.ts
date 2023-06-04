b65fe8add81888ef6cfb0417666cf8791a596f09
Juhyun <boochickenood@gmail.com>
2023-06-04 21:05:27 +0900

    [FEAT] 상수 PAGE_LIST 추가
    url을 상수로 사용하기 위함

---
+++ constants/routers.ts
@@ -0,0 +1,10 @@
+export const PAGE_LIST = {
+  HOME: '/',
+  PROFILE_SETTING: '/setting/profile',
+  PROJECT_MANAGEMENT: '/project/management',
+  DASTHBOARD: '/dashboard',
+  SOCIAL_HOME: '/social/home',
+  SOCIAL_CREATE: '/social/review/write',
+  SOCIAL_FOLLOWER: '/social/follower',
+  SOCIAL_PROFILE: '/social/user',
+};
