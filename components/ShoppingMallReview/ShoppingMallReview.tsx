2853ac3651fa1f04a8e1340efffc23c829596142
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-06-07 16:48:12 +0900

    [REFACTOR] api 요청 하위 컴포넌트로 분리

--- components/ShoppingMallReview/ShoppingMallReview.tsx
+++ components/ShoppingMallReview/ShoppingMallReview.tsx
@@ -6,34 +6,13 @@ import {
   Reviews,
 } from './@index';
 import * as S from './ShoppingMallReview.styles';
-import {
-  ShoppingMallReviewDetail,
-  ShoppingMallReviewInfo,
-} from 'typings/reviews';
-import { useRouter } from 'next/router';
-import { useRecoilState } from 'recoil';
-import { atomReviewSortButtonState } from 'states/atomReveiw';
-import { replaceUrlProtocool } from 'constants/regExp';
-import { RegisterProjectParams } from 'typings/register';
-import { useRegisterShoppingMallProduct } from 'hooks/queries/shopping';
-import {
-  useGetShoppingMallReviewInfo,
-  useGetShoppingMallReviewList,
-} from 'hooks/queries/reviews';
 
 interface ShoppingMallReviewProps {
   projectName: string;
   productURL: string;
   title: string;
   image: string;
-  // shoppingmallReviewInfoData: ShoppingMallReviewInfo;
-  // shoppingmallReviewList: ShoppingMallReviewDetail[];
 }
-
-//   {
-//   shoppingmallReviewInfoData,
-//   shoppingmallReviewList,
-// }: ShoppingMallReviewProps
 
 const ShoppingMallReview = ({
   projectName,
@@ -41,85 +20,43 @@ const ShoppingMallReview = ({
   title,
   image,
 }: ShoppingMallReviewProps) => {
-  const router = useRouter();
-
-  const [searchOption, setSearchOption] = useRecoilState(
-    atomReviewSortButtonState
+  return (
+    <S.Layout>
+      <S.ComprehensiveRatesLayout>
+        <ComprehensiveRates
+          projectName={projectName}
+          productURL={productURL}
+          title={title}
+          image={image}
+        />
+      </S.ComprehensiveRatesLayout>
+      <S.KeywordRatesLayout>
+        <KeywordRates />
+      </S.KeywordRatesLayout>
+      <S.ReviewHeaderLayout>
+        <ReviewHeader
+        // handleSetSearchOption={handleSetSearchOption}
+        />
+      </S.ReviewHeaderLayout>
+      <S.ReviewsLayout>
+        <Reviews
+          projectName={projectName}
+          productURL={productURL}
+          title={title}
+          image={image}
+        />
+      </S.ReviewsLayout>
+      <S.LinkButton
+        href={{
+          pathname: '/review/write',
+          query: { productURL, title },
+        }}
+        as={`/review/write/${title}`}
+      >
+        리뷰작성
+      </S.LinkButton>
+    </S.Layout>
   );
-
-  const handleSetSearchOption = (selectingOption: 'BEST' | 'NEWEST') => {
-    setSearchOption({ selected: selectingOption });
-  };
-
-  const { data: shoppingmallReviewInfoData } =
-    useGetShoppingMallReviewInfo(productURL);
-  const { data: shoppingmallReviewList } = useGetShoppingMallReviewList(
-    productURL,
-    searchOption.selected
-  );
-
-  const { mutateAsync } = useRegisterShoppingMallProduct();
-
-  useEffect(() => {
-    if (
-      shoppingmallReviewInfoData?.status === 202 &&
-      router.query.projectName &&
-      router.query.productURL &&
-      router.query.title &&
-      router.query.image
-    ) {
-      const params: RegisterProjectParams = {
-        projectName: projectName,
-        body: {
-          productUrl: productURL,
-          productName: title,
-          imageUrl: replaceUrlProtocool(image),
-        },
-      };
-
-      mutateAsync(params);
-    }
-  }, [
-    shoppingmallReviewInfoData?.status,
-    router.query,
-    mutateAsync,
-    projectName,
-    productURL,
-    title,
-    image,
-  ]);
-
-  if (shoppingmallReviewInfoData?.data && shoppingmallReviewList?.data) {
-    return (
-      <S.Layout>
-        <S.ComprehensiveRatesLayout>
-          <ComprehensiveRates
-            shoppingmallReviewInfoData={shoppingmallReviewInfoData.data}
-          />
-        </S.ComprehensiveRatesLayout>
-        <S.KeywordRatesLayout>
-          <KeywordRates />
-        </S.KeywordRatesLayout>
-        <S.ReviewHeaderLayout>
-          <ReviewHeader handleSetSearchOption={handleSetSearchOption} />
-        </S.ReviewHeaderLayout>
-        <S.ReviewsLayout>
-          <Reviews shoppingmallReviewList={shoppingmallReviewList.data} />
-        </S.ReviewsLayout>
-        <S.LinkButton
-          href={{
-            pathname: '/review/write',
-            query: { productURL, title },
-          }}
-          as={`/review/write/${title}`}
-        >
-          리뷰작성
-        </S.LinkButton>
-      </S.Layout>
-    );
-  }
-
-  return null;
 };
 
 export default ShoppingMallReview;
