e7a0261506863071d77a0e7683d7a23549115198
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-06-07 16:47:09 +0900

    [CHORE] 안쓰는 import 제거

--- hooks/queries/reviews.ts
+++ hooks/queries/reviews.ts
@@ -1,11 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
 import { shoppingAPI } from 'api/reviews';
-import { AxiosError, AxiosResponse } from 'axios';
-import { ResponseError } from 'typings/error';
-import {
-  ShoppingMallReviewDetail,
-  ShoppingMallReviewInfo,
-} from 'typings/reviews';
 
 export const useGetShoppingMallReviewInfo = (productURL: string) => {
   return useQuery(
