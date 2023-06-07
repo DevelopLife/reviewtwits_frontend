8dd4e778ee2c96499ed7dae72a11a82b828443f1
YeongMin Ahn <ahn0min95@gmail.com>
2023-06-07 17:18:50 +0900

    [FEAT] 통계페이지 일간, 주간, 월간 버튼 추가

--- components/Statistics/ViewsStatistics.tsx
+++ components/Statistics/ViewsStatistics.tsx
@@ -1,19 +1,24 @@
 import { WrapProps } from 'typings/wrapperProps';
 import Shadow from './common/Shadow';
 import * as S from './ViewsStatistics.styles';
+import { MouseEventHandler } from 'react';
 // import SimpleBarChart from 'components/common/Charts/BarChart';
 
-const ViewsStatistics = ({ children }: WrapProps) => {
+interface ViewsStatisticsProps extends WrapProps {
+  onClick: MouseEventHandler;
+}
+
+const ViewsStatistics = ({ children, onClick }: ViewsStatisticsProps) => {
   return (
     <Shadow boxWidth={1440} boxHeight={710}>
       <S.Container>
         <S.StatisticsTitle>일간 조회수</S.StatisticsTitle>
         <S.StatisticsHeader>
           <S.StatisticsSubTitle>2023.02.15 수</S.StatisticsSubTitle>
-          <div>
-            <button>일간</button>
-            <button>주간</button>
-            <button>월간</button>
+          <div onClick={onClick}>
+            <button name="day">일간</button>
+            <button name="week">주간</button>
+            <button name="month">월간</button>
           </div>
         </S.StatisticsHeader>
         <S.GraphBox>{children}</S.GraphBox>
