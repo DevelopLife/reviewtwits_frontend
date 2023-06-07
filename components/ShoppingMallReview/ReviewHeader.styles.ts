1700ceaeccefe71e83fd3102d48dd1d5ec610a17
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-06-07 16:50:02 +0900

    [STYLE] 최신순, 베스트순 버튼 선택 시 스타일 변경

--- components/ShoppingMallReview/ReviewHeader.styles.ts
+++ components/ShoppingMallReview/ReviewHeader.styles.ts
@@ -31,11 +31,23 @@ const SortReviewOptions = styled.div`
   line-height: 19px;
 
   cursor: pointer;
-
-  color: #ffffff;
 `;
 
-const BestReview = styled.div``;
+const BestReview = styled.div<{ isSelected: boolean }>`
+  &:hover {
+    scale: 1.05;
+  }
+  color: ${({ isSelected, theme }) =>
+    isSelected ? theme.colors.secondary : theme.colors.white};
+`;
+
+const LatestReview = styled.div<{ isSelected: boolean }>`
+  &:hover {
+    scale: 1.05;
+  }
+  color: ${({ isSelected, theme }) =>
+    isSelected ? theme.colors.secondary : theme.colors.white};
+`;
 
 const Line = styled.div`
   width: 0px;
@@ -43,8 +55,6 @@ const Line = styled.div`
 
   border: 1px solid #878d91;
 `;
-
-const LatestReview = styled.div``;
 
 const SearchAndRateOptions = styled.div`
   width: 570px;
