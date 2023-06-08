96d06574046709366a1239a3a289962862e8705f
YeongMin Ahn <ahn0min95@gmail.com>
2023-06-08 15:37:23 +0900

    [FEAT] 방문자 카운팅 통계 기능 추가

--- components/Statistics/VisitorStatistics.tsx
+++ components/Statistics/VisitorStatistics.tsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import { UseQueryResult } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+
 import Shadow from './common/Shadow';
 import * as S from './VisitorStatistics.styles';
 import { RecentVisitCounts } from 'typings/statistics';
 
-type VisitorStatisticsProps = Partial<RecentVisitCounts>;
+type VisitorStatisticsProps = Partial<RecentVisitCounts> & {
+  getRecentVisitCounts: UseQueryResult<
+    AxiosResponse<RecentVisitCounts, any>,
+    unknown
+  >;
+};
 
 const VisitorStatistics = ({
-  todayVisit = 0,
-  yesterdayVisit = 0,
-  totalVisit = 0,
+  getRecentVisitCounts,
 }: VisitorStatisticsProps) => {
+  const { data: recentVisitCounts, isLoading } = getRecentVisitCounts;
+
+  // if (isLoading) return null;
+
   return (
     <Shadow boxWidth={682} boxHeight={200}>
       <S.Container>
         <S.VisitorInfoBox>
           <S.VisitorInfo>오늘 방문자수</S.VisitorInfo>
-          <S.VisitorTotalNumber>{todayVisit}</S.VisitorTotalNumber>
+          <S.VisitorTotalNumber>
+            {recentVisitCounts?.data.todayVisit}
+          </S.VisitorTotalNumber>
         </S.VisitorInfoBox>
         <S.VisitorInfoBox>
           <S.VisitorInfo>어제 방문수</S.VisitorInfo>
-          <S.VisitorTotalNumber>{yesterdayVisit}</S.VisitorTotalNumber>
+          <S.VisitorTotalNumber>
+            {recentVisitCounts?.data.yesterdayVisit}
+          </S.VisitorTotalNumber>
         </S.VisitorInfoBox>
         <S.VisitorInfoBox>
           <S.VisitorInfo>누적 방문수</S.VisitorInfo>
           <S.VisitorTotalNumber pointColor="blue_1">
-            {totalVisit}
+            {recentVisitCounts?.data.totalVisit}
           </S.VisitorTotalNumber>
         </S.VisitorInfoBox>
       </S.Container>
