399c2b106f959681a02d1e32470f098ad22fcd6d
YeongMin Ahn <ahn0min95@gmail.com>
2023-06-09 01:43:31 +0900

    [REFACTOR] page단에서 useRouter를 이용하여 query params 넘기도록 변경

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -5,18 +5,21 @@ import {
 } from 'components/Dashboard/@index';
 import DashboardPageLayout from 'components/Dashboard/common/DashboardPageLayout';
 import Margin from 'components/Dashboard/common/Margin';
-import React from 'react';
+// import VisitorStatistics from 'components/Statistics/VisitorStatistics';
+import { useRouter } from 'next/router';
 
 const DashboardPage = () => {
+  const router = useRouter();
+  const { projectId } = router.query as { projectId: string };
+
   return (
     <DashboardPageLayout>
       <Margin marginTop={60} marginBottom={51}>
-        <ComprehensiveData />
+        <ComprehensiveData projectId={projectId} />
       </Margin>
       <Graph />
-
       <Margin marginTop={70.58} marginBottom={60}>
-        <OrderData />
+        <OrderData projectId={projectId} />
       </Margin>
     </DashboardPageLayout>
   );
