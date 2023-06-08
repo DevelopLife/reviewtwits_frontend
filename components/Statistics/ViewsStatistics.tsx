0f479293d33f33f4c1cf63c4757d5247bd3cf12f
YeongMin Ahn <ahn0min95@gmail.com>
2023-06-08 15:37:51 +0900

    [FEAT] 내부적으로 BarChart를 통해 방문자 통계를 보여주는 기능 추가

--- components/Statistics/ViewsStatistics.tsx
+++ components/Statistics/ViewsStatistics.tsx
@@ -1,27 +1,54 @@
-import { WrapProps } from 'typings/wrapperProps';
+import { useState } from 'react';
 import Shadow from './common/Shadow';
 import * as S from './ViewsStatistics.styles';
-import { MouseEventHandler } from 'react';
-// import SimpleBarChart from 'components/common/Charts/BarChart';
 
-interface ViewsStatisticsProps extends WrapProps {
-  onClick: MouseEventHandler;
-}
+import SimpleBarChart from 'components/common/Charts/SimpleBarChart';
+import useVisitorChart from 'hooks/useVisitorChart';
 
-const ViewsStatistics = ({ children, onClick }: ViewsStatisticsProps) => {
+const ViewsStatistics = () => {
+  // TODO: 하루 간격 한달 범위
+  const [mo, setMo] = useState({
+    range: '1mo',
+    interval: '1d',
+  });
+
+  const {
+    referenceDate,
+    focusedDate,
+    visitInfos,
+    onClickPrevButton,
+    onClickNextButton,
+    onClickBar,
+  } = useVisitorChart();
+
   return (
     <Shadow boxWidth={1440} boxHeight={710}>
       <S.Container>
         <S.StatisticsTitle>일간 조회수</S.StatisticsTitle>
         <S.StatisticsHeader>
-          <S.StatisticsSubTitle>2023.02.15 수</S.StatisticsSubTitle>
-          <div onClick={onClick}>
+          <S.StatisticsSubTitle>
+            {focusedDate.toLocaleDateString()}
+          </S.StatisticsSubTitle>
+          <div onClick={() => void 0}>
+            {
+              //TODO: 버튼 클릭 시 간격 state 조정하기
+            }
             <button name="day">일간</button>
             <button name="week">주간</button>
             <button name="month">월간</button>
           </div>
         </S.StatisticsHeader>
-        <S.GraphBox>{children}</S.GraphBox>
+        <S.GraphBox>
+          {visitInfos?.length ? (
+            <SimpleBarChart
+              data={visitInfos}
+              focusedDate={focusedDate}
+              onClickLeftButton={onClickPrevButton}
+              onClickRightButton={onClickNextButton}
+              onClickBar={onClickBar}
+            />
+          ) : null}
+        </S.GraphBox>
       </S.Container>
     </Shadow>
   );
