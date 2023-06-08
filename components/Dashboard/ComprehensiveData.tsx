27f87db2b035d4f27efe27daf08e5380abb7334f
YeongMin Ahn <ahn0min95@gmail.com>
2023-06-09 01:40:26 +0900

    [REFACTOR] dashboard page 항목별 카운팅 컴포넌트 변경

--- components/Dashboard/ComprehensiveData.tsx
+++ components/Dashboard/ComprehensiveData.tsx
@@ -1,49 +1,70 @@
-import React from 'react';
-import Image from 'next/image';
+import type { FC, SVGProps } from 'react';
+
+import ReactIcon from 'public/icons/react.svg';
 
 import Shadow from './common/Shadow';
 import * as S from './ComprehensiveData.styles';
 
-const ComprehensiveData = () => {
+type ProjectCountingInformations = {
+  monthViews: number;
+  dailyReviews: number;
+  pendingReviews: number;
+  registeredProducts: number;
+};
+
+type CountingInformationKey = keyof ProjectCountingInformations;
+
+type CountingInformationValue = {
+  Icon: FC<SVGProps<SVGSVGElement>>;
+  title: string;
+};
+
+type CountingInformationsTuple = [
+  CountingInformationKey,
+  CountingInformationValue
+];
+
+const COUNTING_INFORMATIONS = {
+  monthViews: { Icon: ReactIcon, title: '월간 조회수' },
+  dailyReviews: { Icon: ReactIcon, title: '일별 리뷰수' },
+  pendingReviews: { Icon: ReactIcon, title: '대기중인 리뷰' },
+  registeredProducts: { Icon: ReactIcon, title: '등록된 상품수' },
+};
+
+interface ComprehensiveDataProps {
+  projectId: string;
+}
+
+const ComprehensiveData = ({ projectId }: ComprehensiveDataProps) => {
+  // TODO: useHooks
+  // const { data } = useGetCountingInformations(projectId)
+  // const { monthViews, dailyReviews, pendingReviews, registeredProducts } = data;
+
+  const mockData: ProjectCountingInformations = {
+    monthViews: 0,
+    dailyReviews: 0,
+    pendingReviews: 0,
+    registeredProducts: 0,
+  };
+  const countingInformations = Object.entries(
+    COUNTING_INFORMATIONS
+  ) as Array<CountingInformationsTuple>;
+
   return (
     <S.Container>
-      <Shadow boxSize="SMALL">
-        <S.Card>
-          <Image width={30} height={30} src={''} alt={'mock'} />
-          <div>
-            <S.CardTitle>일별 조회수</S.CardTitle>
-            <S.CardDesc>1,504</S.CardDesc>
-          </div>
-        </S.Card>
-      </Shadow>
-      <Shadow boxSize="SMALL">
-        <S.Card>
-          <Image width={30} height={30} src={''} alt={'mock'} />
-          <div>
-            <S.CardTitle>대기중인 리뷰</S.CardTitle>
-            <S.CardDesc>32</S.CardDesc>
-          </div>
-        </S.Card>
-      </Shadow>
-      <Shadow boxSize="SMALL">
-        <S.Card>
-          <Image width={30} height={30} src={''} alt={'mock'} />
-          <S.Orders>
-            <S.Order>
-              <S.OrderState>배송준비</S.OrderState>
-              <S.OrderCount>448건</S.OrderCount>
-            </S.Order>
-            <S.Order>
-              <S.OrderState>배송중</S.OrderState>
-              <S.OrderCount>376건</S.OrderCount>
-            </S.Order>
-            <S.Order>
-              <S.OrderState>배송완료</S.OrderState>
-              <S.OrderCount>1314건</S.OrderCount>
-            </S.Order>
-          </S.Orders>
-        </S.Card>
-      </Shadow>
+      {mockData && countingInformations.length
+        ? countingInformations.map(([key, { Icon, title }]) => (
+            <Shadow boxSize="SMALL" key={title}>
+              <S.Card>
+                <Icon />
+                <div>
+                  <S.CardTitle>{title}</S.CardTitle>
+                  <S.CardDesc>{mockData[key]}</S.CardDesc>
+                </div>
+              </S.Card>
+            </Shadow>
+          ))
+        : null}
     </S.Container>
   );
 };
