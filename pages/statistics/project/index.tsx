d2a653de9d950def8ad97a2c44b74e105c780365
YeongMin Ahn <ahn0min95@gmail.com>
2023-06-08 15:38:21 +0900

    [FEAT] 현재 사용되지 않은 차트 컴포넌트 주석처리

--- pages/statistics/project/index.tsx
+++ pages/statistics/project/index.tsx
@@ -1,14 +1,13 @@
+import { useRouter } from 'next/router';
+
 import {
   VisitorStatistics,
   ViewsStatistics,
-  LeadTimeRate,
 } from 'components/Statistics/@index';
 import Margin from 'components/Statistics/common/Margin';
 import StatisticsPageLayout from 'components/Statistics/common/StatisticsPageLayout';
-import SimpleBarChart from 'components/common/Charts/BarChart';
-import useVisitChart from 'components/common/Charts/BarChartState';
+
 import useStatistics from 'hooks/queries/statistics';
-import { useRouter } from 'next/router';
 
 const ProjectStatisticsPage = () => {
   const router = useRouter();
@@ -24,34 +23,22 @@ const ProjectStatisticsPage = () => {
     // interval,
   });
 
-  const { data: recentVisitCounts } = recentVisitCountsQuery;
   const { data: dailyVisitGraphInfos } = dailyVisitGraphInfosQuery;
   const { data: visitGraphInfos } = visitGraphInfosQuery;
-
-  const { origin, handleSelectedValueOnClick } = useVisitChart({
-    createInitialStateParams: {
-      interval: 7,
-      elementCount: 20,
-    },
-  });
 
   return (
     <StatisticsPageLayout>
       <Margin marginTop={60}>
-        <VisitorStatistics {...recentVisitCounts?.data} />
+        <VisitorStatistics getRecentVisitCounts={recentVisitCountsQuery} />
       </Margin>
       <Margin marginTop={40}>
-        <ViewsStatistics onClick={handleSelectedValueOnClick}>
-          {dailyVisitGraphInfos?.data?.visitInfo.length ? (
-            <SimpleBarChart data={dailyVisitGraphInfos?.data?.visitInfo} />
-          ) : null}
-        </ViewsStatistics>
+        <ViewsStatistics />
       </Margin>
       <Margin marginTop={48}>
-        <LeadTimeRate
+        {/* <LeadTimeRate
           chartDatas={visitGraphInfos?.data}
           dataKey={'previousVisit'}
-        />
+        /> */}
       </Margin>
 
       <Margin marginTop={32}>{/* <ProductStatistics /> */}</Margin>
