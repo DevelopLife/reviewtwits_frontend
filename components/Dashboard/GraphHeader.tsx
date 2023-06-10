87b5732effeaa48fb6a1abd2514524eb0631060d
YeongMin Ahn <ahn0min95@gmail.com>
2023-06-10 21:45:19 +0900

    [FEAT] dashboard 그래프 상단 방문자 카운트 구현

--- components/Dashboard/GraphHeader.tsx
+++ components/Dashboard/GraphHeader.tsx
@@ -1,29 +1,53 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
+import useStatistics from 'hooks/queries/statistics';
 import Button from './common/Button';
 import * as S from './GraphHeader.styles';
 
-const PageData = () => {
+interface PageDataProps {
+  projectId: string;
+}
+
+const PageData = ({ projectId }: PageDataProps) => {
+  const router = useRouter();
+  const { query } = router;
+
+  const { useVisitGraphInfosQuery } = useStatistics({
+    projectId,
+  });
+
+  const { data } = useVisitGraphInfosQuery();
+  const visitCountData = data?.data;
+
   return (
     <S.Container>
       <S.Datas>
         <S.Data>
           <S.DataTitle>오늘 방문수</S.DataTitle>
-          <S.DataDesc>98</S.DataDesc>
+          <S.DataDesc>{visitCountData?.todayVisit || 0}</S.DataDesc>
         </S.Data>
         <S.Data>
           <S.DataTitle>어제 방문수</S.DataTitle>
-          <S.DataDesc>407</S.DataDesc>
+          <S.DataDesc>{visitCountData?.yesterdayVisit || 0}</S.DataDesc>
         </S.Data>
         <S.Data>
           <S.DataTitle>누적 방문수</S.DataTitle>
-          <S.DataDesc>571,146</S.DataDesc>
+          <S.DataDesc>{visitCountData?.totalVisit || 0}</S.DataDesc>
         </S.Data>
       </S.Datas>
-      <Button>
-        방문 통계
-        <Image width={30} height={30} src="" alt="mock" />
-      </Button>
+      <Link
+        href={{
+          pathname: '/statistics/project',
+          query,
+        }}
+      >
+        <Button>
+          방문 통계
+          <Image width={30} height={30} src="" alt="mock" />
+        </Button>
+      </Link>
     </S.Container>
   );
 };
