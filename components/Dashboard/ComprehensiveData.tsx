b66419d95d2e88d01e2fbae299fc0e1bdc02ade9
YeongMin Ahn <ahn0min95@gmail.com>
2023-06-10 21:44:03 +0900

    [FEAT] dashboard에서 메인 데이터를 보여주는 영역 구현

--- components/Dashboard/ComprehensiveData.tsx
+++ components/Dashboard/ComprehensiveData.tsx
@@ -1,15 +1,17 @@
 import type { FC, SVGProps } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-import ReactIcon from 'public/icons/react.svg';
-
+import OnePeopleIcon from 'public/icons/one-people.svg';
+import MultiPeopleIcon from 'public/icons/multi-people.svg';
+import { statisticsAPI } from 'api/statistics';
 import Shadow from './common/Shadow';
 import * as S from './ComprehensiveData.styles';
 
 type ProjectCountingInformations = {
-  monthViews: number;
-  dailyReviews: number;
-  pendingReviews: number;
-  registeredProducts: number;
+  monthlyVisitCount: number;
+  dailyReviewCount: number;
+  pendingReviewCount: number;
+  registeredProductCount: number;
 };
 
 type CountingInformationKey = keyof ProjectCountingInformations;
@@ -25,10 +27,10 @@ type CountingInformationsTuple = [
 ];
 
 const COUNTING_INFORMATIONS = {
-  monthViews: { Icon: ReactIcon, title: '월간 조회수' },
-  dailyReviews: { Icon: ReactIcon, title: '일별 리뷰수' },
-  pendingReviews: { Icon: ReactIcon, title: '대기중인 리뷰' },
-  registeredProducts: { Icon: ReactIcon, title: '등록된 상품수' },
+  monthlyVisitCount: { Icon: MultiPeopleIcon, title: '월간 조회수' },
+  dailyReviewCount: { Icon: OnePeopleIcon, title: '일별 리뷰수' },
+  pendingReviewCount: { Icon: OnePeopleIcon, title: '대기중인 리뷰' },
+  registeredProductCount: { Icon: OnePeopleIcon, title: '등록된 상품수' },
 };
 
 interface ComprehensiveDataProps {
@@ -37,29 +39,37 @@ interface ComprehensiveDataProps {
 
 const ComprehensiveData = ({ projectId }: ComprehensiveDataProps) => {
   // TODO: useHooks
-  // const { data } = useGetCountingInformations(projectId)
-  // const { monthViews, dailyReviews, pendingReviews, registeredProducts } = data;
 
-  const mockData: ProjectCountingInformations = {
-    monthViews: 0,
-    dailyReviews: 0,
-    pendingReviews: 0,
-    registeredProducts: 0,
-  };
+  const { data } = useQuery(
+    ['simpleProjectInfo', projectId],
+    () => statisticsAPI.simpleProjectInfo(projectId),
+    { enabled: !!projectId }
+  );
+  const { data: data1 } = useQuery(
+    ['productStatistics', projectId],
+    () => statisticsAPI.productStatistics(projectId),
+    { enabled: !!projectId }
+  );
+  const { data: data2 } = useQuery(
+    ['requestInflowInfos', projectId],
+    () => statisticsAPI.requestInflowInfos(projectId),
+    { enabled: !!projectId }
+  );
+
   const countingInformations = Object.entries(
     COUNTING_INFORMATIONS
   ) as Array<CountingInformationsTuple>;
 
   return (
     <S.Container>
-      {mockData && countingInformations.length
+      {data && countingInformations.length
         ? countingInformations.map(([key, { Icon, title }]) => (
             <Shadow boxSize="SMALL" key={title}>
               <S.Card>
                 <Icon />
                 <div>
                   <S.CardTitle>{title}</S.CardTitle>
-                  <S.CardDesc>{mockData[key]}</S.CardDesc>
+                  <S.CardDesc>{data[key] || 0}</S.CardDesc>
                 </div>
               </S.Card>
             </Shadow>
