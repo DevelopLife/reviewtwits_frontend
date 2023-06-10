ee197787c6db3f5c2c01e77753ed6d2366d10b2f
YeongMin Ahn <ahn0min95@gmail.com>
2023-06-10 21:44:38 +0900

    [FEAT] dashboard graph 영역 렌더링 적용

--- components/Dashboard/Graph.tsx
+++ components/Dashboard/Graph.tsx
@@ -1,18 +1,57 @@
 import GraphHeader from './GraphHeader';
-import GraphBody from './GraphBody';
 // import GraphSide from './GraphSide';
 import Shadow from './common/Shadow';
 
 import * as S from './Graph.styles';
+import SimpleLineChart from 'components/common/Charts/SimpleLineChart';
+import useStatistics from 'hooks/queries/statistics';
+import { VisitInfo } from 'typings/statistics';
 
-const Graph = () => {
-  // TODO: call use Query
+interface GraphProps {
+  projectId: string;
+}
+
+const Graph = ({ projectId }: GraphProps) => {
+  const { useVisitGraphInfosQuery } = useStatistics({
+    projectId,
+  });
+
+  const { data } = useVisitGraphInfosQuery();
+  const chartData = data?.data.visitInfo;
+
+  function transformData(datas: VisitInfo[]) {
+    return datas.reduce((acc, cur, index) => {
+      const { timeStamp } = cur;
+      const previousVisitInfo = acc[index - 1];
+
+      if (previousVisitInfo) {
+        const { timeStamp: previouseTimeStamp } = acc[index - 1];
+
+        const isDifferenceMonth =
+          new Date(timeStamp).getMonth() !==
+          new Date(previouseTimeStamp).getMonth();
+
+        return [...acc, { ...cur, isDifferenceMonth }];
+      }
+
+      return [...acc, { ...cur, isDifferenceMonth: false }];
+    }, [] as VisitInfo[]);
+  }
+
+  const transformedData = chartData && transformData(chartData);
 
   return (
     <S.Container>
       <Shadow boxSize="MEDIUM">
-        <GraphHeader />
-        <GraphBody />
+        <GraphHeader projectId={projectId} />
+        {chartData?.length && transformedData?.length ? (
+          <SimpleLineChart
+            data={transformedData}
+            dataKeys={['visitCount']}
+            xKeys={['timeStamp']}
+            strokeColors={['red']}
+          />
+        ) : null}
       </Shadow>
       {/* <GraphSide /> */}
     </S.Container>
