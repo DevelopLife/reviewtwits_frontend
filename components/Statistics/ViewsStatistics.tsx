cf1dfb71dfc8e26152020b131729c5da4115c094
YeongMin Ahn <ahn0min95@gmail.com>
2023-07-01 17:47:18 +0900

    Refactoring/styles file (#252)
    * [REFACTOR] components/Statistics 폴더  styles 파일 제거
    
    * [REFACTOR] components/social/common 폴더 styles 파일 제거
    
    * [CHORE] 공용으로 사용되는 View의 TODO 주석 작성

--- components/Statistics/ViewsStatistics.tsx
+++ components/Statistics/ViewsStatistics.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import styled from '@emotion/styled';
+
 import Shadow from './common/Shadow';
-import * as S from './ViewsStatistics.styles';
 
 const ViewsStatistics = () => {
   return (
@@ -11,6 +11,41 @@ const ViewsStatistics = () => {
       </S.Container>
     </Shadow>
   );
+};
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
+    width: 1360px;
+    height: auto;
+
+    padding-top: 52px;
+    padding-left: 40px;
+    padding-right: 40px;
+    padding-bottom: 31.85px;
+  `,
+
+  StatisticsTitle: styled.h3`
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 33px;
+
+    /* Black */
+
+    color: #181818;
+  `,
+
+  GraphBox: styled.div`
+    width: 1360px;
+    height: 577.15px;
+
+    border: 1px solid black;
+  `,
 };
 
 export default ViewsStatistics;
