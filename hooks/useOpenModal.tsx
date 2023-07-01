57536a11e021d27f1fee78f8d44c5696408fd854
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-07-01 17:19:25 +0900

    [FIX] modal

--- hooks/useOpenModal.tsx
+++ hooks/useOpenModal.tsx
@@ -1,26 +1,18 @@
 import MODAL_LIST from 'constants/modal';
 import { useRouter } from 'next/router';
 import useModal from './useModal';
-import { PAGE_LIST } from 'constants/routers';
-
-type ValueOf<T> = T[keyof T];
 
 interface useRouteModalPageProps {
   nickname?: string;
   reviewId?: number;
-  page: ValueOf<typeof PAGE_LIST>;
 }
 
-const useRouteModalPage = ({
-  nickname,
-  reviewId,
-  page,
-}: useRouteModalPageProps) => {
+const useRouteModalPage = ({ nickname, reviewId }: useRouteModalPageProps) => {
   const router = useRouter();
   const modal = useModal();
 
   const routeModalPage = () => {
-    router.push(`${page}?userName=${nickname}&reviewId=${reviewId}`);
+    router.push(`${router.asPath}?userName=${nickname}&reviewId=${reviewId}`);
     modal.show({ key: MODAL_LIST.SOCIAL_FEED_DETAIL });
   };
 
