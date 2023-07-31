d385fc96b9192b859749c4c9b13eeaedc46b2dbd
YeongMin Ahn <ahn0min95@gmail.com>
2023-07-31 20:54:54 +0900

    feat: HTML용 코드 수정가능하게 변경

--- constants/install_guides/code_pen_url.ts
+++ constants/install_guides/code_pen_url.ts
@@ -1,7 +1,7 @@
 import { PlatformUppercases } from 'typings/platforms';
 
 export const CODE_PEN_URLS: Record<PlatformUppercases, string> = {
-  HTML: 'https://codepen.io/ahn0min/embed/rNQZvMZ?default-tab=html%2Cresult',
+  HTML: 'https://codepen.io/ahn0min/embed/preview/rNQZvMZ?default-tab=html%2Cresult&editable=true',
   // TODO: React 용 라이브러리를 cdn으로 배포해서 code pen에서 import 해서 사용합니다.
   REACT: '',
 };
