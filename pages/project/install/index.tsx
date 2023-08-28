7e81aa6eb59d21a163362e646d729dd0aa979a45
YeongMin Ahn <ahn0min95@gmail.com>
2023-08-28 20:00:18 +0900

    Revert "[FEAT] 인스톨 가이드 페이지에 Code Pen 추가 (#265)" (#266)
    This reverts commit 53d7d40326ad9bc562f205fa6d6aced02a866eeb.

--- pages/project/install/index.tsx
+++ pages/project/install/index.tsx
@@ -6,7 +6,6 @@ import { ProjectPageLayout } from 'components/Project/common/ProjectPageLayout';
 import { ProjectCreateLayout } from 'components/Project/Create/ProjectCreateLayout';
 import { PROJECT_TITLE } from 'constants/project';
 import ReactIcon from 'public/icons/react.svg';
-import GlobeIcon from 'public/icons/globe.svg';
 import type { Platforms } from 'typings/platforms';
 
 const platforms: {
@@ -19,7 +18,7 @@ const platforms: {
   },
   {
     name: 'HTML',
-    Icon: GlobeIcon,
+    Icon: ReactIcon,
   },
 ];
 
@@ -39,7 +38,7 @@ const InstallPage = () => {
                 style={{ textDecoration: 'none', color: '#3D3D3D' }}
               >
                 <S.PlatformItem>
-                  <Icon width={150} height={150} />
+                  <Icon />
                   <S.PlatformName marginTop={28}>{name}</S.PlatformName>
                 </S.PlatformItem>
               </Link>
@@ -67,6 +66,9 @@ const S = {
     gap: 102px;
   `,
   PlatformItem: styled.div`
+    width: 132px;
+    height: 168px;
+
     text-align: center;
 
     :hover {
