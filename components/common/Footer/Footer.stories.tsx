0b346cc2f2fafd2c5ce6b9c7fe458f1f4ed39a71
YeongMin Ahn <ahn0min95@gmail.com>
2023-08-29 19:40:06 +0900

    Delete components/common directory

--- components/common/Footer/Footer.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { StoryFn } from '@storybook/react';
-import Footer from './Footer';
-
-export default {
-  title: 'Component/Footer',
-  component: Footer,
-};
-
-export const Basic: StoryFn<typeof Footer> = (args) => <Footer {...args} />;
--- components/common/Modal/modals.ts
+++
@@ -1,1 +0,0 @@
-export { default as SocialFeedModal } from './SocialFeedModal';
