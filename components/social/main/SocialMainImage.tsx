9a651d395c78802afc4a835939eae4771a52035a
YeongMin Ahn <ahn0min95@gmail.com>
2023-08-29 19:40:52 +0900

    Delete components/social directory

--- components/social/main/SocialMainImage.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-import MainImage from 'public/images/social_main_img.jpg';
-
-const SocialMainImage = () => (
-  <Image width={738} height={738} quality={100} src={MainImage} alt="mainImg" />
-);
-
-export default SocialMainImage;
